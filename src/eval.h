8cca9382c9c5e7c2d2099a92c4e21e3ae0a57bbb
LiuTao <mat.liutos@gmail.com>
2012-10-07 18:49:06 +0800

    delete some obsulete files

--- src/eval.h
+++
@@ -1,8 +0,0 @@
-#ifndef EVAL_H
-#define EVAL_H
-
-#include "types.h"
-
-struct LispObject *eval_expression(struct LispObject *, ENVIRONMENT *);
-
-#endif
--- src/print.h
+++
@@ -1,9 +0,0 @@
-#ifndef PRINT_H
-#define PRINT_H
-
-#include "types.h"
-
-extern void print_object(struct LispObject *);
-extern void print_env(ENVIRONMENT *, BOOLEAN);
-
-#endif
