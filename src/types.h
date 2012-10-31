47e3f00dabe1426af94f81bda6045e36f9756995
LiuTao <mat.liutos@gmail.com>
2012-10-31 20:00:09 +0800

    一、增加了struct stream_t结构体类型的定义 二、挪动了大量宏定义的位置并增加了相应的注释区分类别 三、修改了LispObject的定义，给匿名的内部union和struct增加了简单的名字。

--- src/types.h
+++ src/types.h
@@ -1,20 +1,33 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#include <stdio.h>
+
 typedef enum {
     CONS,                       /* Everything except CONS is of type ATOM. */
     SYMBOL,
     INTEGER,
     FUNCTION,
     STRING,
+    STREAM,
 } LispType;
 
-typedef struct LispObject *(*primitive_t)(struct LispObject *);
+struct LispObject;
+typedef struct LispObject *LispObject;
+/* Alias. For readbility. */
+typedef LispObject Cons;
+typedef LispObject Atom;
+typedef LispObject Symbol;
+typedef LispObject Boolean;
+typedef LispObject Function;
+typedef LispObject (*primitive_t)(LispObject);
+typedef LispObject Stream;
 
 typedef int BOOL;
 #define TRUE 1
 #define FALSE 0
 
+/* Function definition. */
 typedef struct function_t {
     BOOL is_c_func;
     union {
@@ -22,54 +35,88 @@ typedef struct function_t {
 	struct {
 	    struct LispObject *parms;
 	    struct LispObject *expr;
-	    struct Environment *env;
-	    struct Environment *denv;
+	    struct Environment *env; /* Lexical Bindings */
+	    struct Environment *denv; /* Dynamic Bindings */
+            struct block_environment *block_env; /* Block Environment */
+            struct Environment *fenv;            /* Function definitions. */
 	};
     };
 } *function_t;                  /* The inner representation of the
                                    function type in Lisp level. */
-typedef struct LispObject {
+/* Symbol definition */
+struct symbol_t;
+typedef struct symbol_t *symbol_t;
+struct symbol_t {
+    char *symbol_name;
+    LispObject value_cell;
+    LispObject function_cell;
+};
+/* Stream definition */
+typedef enum {
+    FILE_STREAM,
+    CHARACTER_STREAM,
+    BYTE_STREAM,
+} STREAM_TYPE;
+
+struct stream_t;
+typedef struct stream_t *stream_t;
+typedef stream_t file_stream;
+typedef stream_t char_stream;
+typedef stream_t byte_stream;
+struct stream_t {
+    STREAM_TYPE type;
+    union {
+        FILE *file;
+    } u;
+};
+
+/* Lisp Object definition */
+struct LispObject {
     LispType type;
     union {
-	char *symbol_name;
+	struct symbol_t *symbol;
 	int integer;
 	function_t fun;
         char *string;
+        struct stream_t *stream;
 	struct {
-	    struct LispObject *car;
-	    struct LispObject *cdr;
-	};
-    };
-} *LispObject;
+	    LispObject car;
+	    LispObject cdr;
+	} s;
+    } u;
+};
 
-/* Alias. For readbility. */
-typedef LispObject Cons;
-typedef LispObject Atom;
-typedef LispObject Symbol;
-typedef LispObject Function;
-typedef LispObject Boolean;
-
-#define CAR(x) ((x)->car)
-#define CDR(x) ((x)->cdr)
+/* Object */
+#define TYPE(x) ((x)->type)
+/* Cons Object */
+#define CAR(x) ((x)->u.s.car)
+#define CDR(x) ((x)->u.s.cdr)
 #define SCAR(x) safe_car(x)     /* A safe version of macro CAR */
 #define SCDR(x) safe_cdr(x)     /* A safe version of macro CDR */
-#define TYPE(x) ((x)->type)
-#define INTEGER(x) ((x)->integer)
-#define FUNCTION(x) ((x)->fun)
-#define PRIMITIVE(x) (FUNCTION(x)->prim)
-#define EXPRESSION(x) ((x)->fun->expr)
-#define PARAMETERS(x) ((x)->fun->parms)
-#define LOCAL_ENV(x) ((x)->fun->env)
-#define FUNC_FLAG(x) ((x)->fun->is_c_func)
-#define FUNC_DENV(x) (FUNCTION(x)->denv)
-
-#define PHEAD(func_name) LispObject func_name(Cons args)
-
 #define FIRST(x) CAR(x)
 #define SECOND(x) CAR(SCDR(x))
 #define CDDR(x) SCDR(SCDR(x))
 #define THIRD(x) SCAR(CDDR(x))
 #define FOURTH(x) SCAR(SCDR(CDDR(x)))
+/* Integer Object */
+#define INTEGER(x) ((x)->u.integer)
+/* Function Object */
+#define FUNCTION(x) ((x)->u.fun)
+#define LOCAL_ENV(x) (FUNCTION(x)->env)
+#define PRIMITIVE(x) (FUNCTION(x)->prim)
+#define FUNC_DENV(x) (FUNCTION(x)->denv)
+#define FUNC_FLAG(x) (FUNCTION(x)->is_c_func)
+#define BLOCK_ENV(x) (FUNCTION(x)->block_env)
+#define EXPRESSION(x) (FUNCTION(x)->expr)
+#define PARAMETERS(x) (FUNCTION(x)->parms)
+#define FENV(x) (FUNCTION(x)->fenv)
+/* Symbol Object */
+#define theSYMBOL(x) ((x)->u.symbol)
+#define SYMBOL_NAME(x) (theSYMBOL(x)->symbol_name)
+/* String Object */
+#define STRING(x) ((x)->u.string)
+
+#define PHEAD(func_name) LispObject func_name(Cons args)
 
 extern BOOL is_debug_mode;
 
