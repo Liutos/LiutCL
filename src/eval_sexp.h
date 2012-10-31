f509119f083b4f130e90b6ec77a717cf1135537c
LiuTao <mat.liutos@gmail.com>
2012-10-31 19:58:40 +0800

    修改eval_sexp函数声明以符合eval_sexp.c文件的定义

--- src/eval_sexp.h
+++ src/eval_sexp.h
@@ -4,7 +4,7 @@
 #include "types.h"
 #include "env_types.h"
 
-extern LispObject eval_sexp(Cons, Environment, Environment);
+extern LispObject eval_sexp(Cons, Environment, Environment, BlockEnvironment, Environment);
 extern void init_special_operators(void);
 
 #endif
