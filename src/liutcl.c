53427fc2cac6f30625f4aaa10af9676152e7cfa0
LiuTao <mat.liutos@gmail.com>
2012-10-31 19:55:04 +0800

    新增了block_env变量，用于存储块环境，顶层的块环境显然为NULL。

--- src/liutcl.c
+++ src/liutcl.c
@@ -1,7 +1,7 @@
 /*
  * liutcl.c
  *
- *
+ * The implementation of the toplevel interface.
  *
  * Copyright (C) 2012-10-15 liutos mat.liutos@gmail.com
  */
@@ -19,18 +19,23 @@ int main(int argc, char *argv[])
 {
     Environment lexical_env;    /* Lexical environment */
     Environment dynamic_env;    /* Dynamic environment */
+    Environment fun_env;        /* Function definition environment. */
     char *expr;
     LispObject sexp;
+    BlockEnvironment block_env; /* The block environment. */
 
     init_symbol_table();
     lexical_env = new_env();
     lexical_env = init_variables(lexical_env);
     dynamic_env = new_env();
-    dynamic_env = init_primitives(dynamic_env);
+    /* dynamic_env = init_primitives(dynamic_env); */
+    fun_env = new_env();
+    fun_env = init_primitives(fun_env);
+    block_env = NULL;
     do {
         expr = read_sexp(stdin);
         sexp = parse_sexp(expr);
-        print_sexp(eval_sexp(sexp, lexical_env, dynamic_env));
+        print_sexp(eval_sexp(sexp, lexical_env, dynamic_env, block_env, fun_env));
     } while (1);
 
     return 0;
