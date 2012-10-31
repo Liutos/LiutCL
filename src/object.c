7c17718bae3938c37fa780cf443c00f1178191d5
LiuTao <mat.liutos@gmail.com>
2012-10-31 19:55:38 +0800

    只是分开了对两个头文件的include指令而已

--- src/object.c
+++ src/object.c
@@ -1,11 +1,12 @@
 /*
  * object.c
  *
- *
+ * Some operations common among all type of objects.
  *
  * Copyright (C) 2012-10-05 liutos
  */
 #include "types.h"
+
 #include <stdlib.h>
 
 LispObject new_object(void)
