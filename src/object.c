3c503ffc2f15103d7e5ce05633f1a2a7d9cc806a
LiuTao <mat.liutos@gmail.com>
2012-11-01 22:30:05 +0800

    一、修改make_symbol为make_C_symbol 二、增加了make_C_file_stream和make_C_string_stream两个函数

--- src/object.c
+++ src/object.c
@@ -8,9 +8,41 @@
 #include "types.h"
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 LispObject new_object(void)
 {
     return malloc(sizeof(struct LispObject));
 }
 
+symbol_t make_C_symbol(char *symbol_name)
+{
+    symbol_t symbol = malloc(sizeof(struct symbol_t));
+
+    symbol->symbol_name = symbol_name;
+    symbol->value_cell = NULL;
+    symbol->function_cell = NULL;
+
+    return symbol;
+}
+
+stream_t make_C_file_stream(FILE *fp)
+{
+    stream_t stream = malloc(sizeof(struct stream_t));
+
+    stream->type = FILE_STREAM;
+    stream->u.file = fp;
+
+    return stream;
+}
+
+stream_t make_C_string_stream(char *string)
+{
+    stream_t stream = malloc(sizeof(struct stream_t));
+
+    stream->type = CHARACTER_STREAM;
+    stream->u.string = strdup(string);
+
+    return stream;
+}
