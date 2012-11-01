f6d62a19837294e43be887f14110ff426fa85ec2
LiuTao <mat.liutos@gmail.com>
2012-11-01 22:27:33 +0800

    一、make_file_stream负责创建文件流对象，make_string_stream负责创建字符串流对象； 二、read_stream_char从流对象中读取并返回一个Character对象，其支撑函数目前仅有read_file_stream_char，负责从文件流中读取。 三、write_stream_char和read_stream_char差不多，只是功能为写入而不是读取。

--- src/stream.c
+++ src/stream.c
@@ -7,26 +7,74 @@
  */
 #include "types.h"
 #include "object.h"
+#include "atom_proc.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
-stream_t _make_file_stream(FILE *fp)
-{
-    stream_t stream = malloc(sizeof(struct stream_t));
-
-    stream->type = FILE_STREAM;
-    stream->u.file = fp;
-
-    return stream;
-}
+Stream standard_output;         /* Stream object forwards standard output. */
+Stream standard_input;          /* Stream object forwards standard input. */
 
 Stream make_file_stream(FILE *fp)
 {
     Stream object = new_object();
 
     TYPE(object) = STREAM;
-    object->u.stream = _make_file_stream(fp);
+    theSTREAM(object) = make_C_file_stream(fp);
 
     return object;
+}
+
+Stream make_string_stream(char *string)
+{
+    Stream object = new_object();
+
+    TYPE(object) = STREAM;
+    theSTREAM(object) = make_C_string_stream(string);
+
+    return object;
+}
+
+Character read_file_stream_char(Stream file_stream)
+{
+    return make_char(fgetc(theSTREAM(file_stream)->u.file));
+}
+
+Character read_stream_char(Stream stream)
+{
+    switch (theSTREAM(stream)->type) {
+    case FILE_STREAM:
+        return read_file_stream_char(stream);
+        break;
+    default :
+        fprintf(stderr, "Unknown stream type.\n");
+        exit(1);
+    }
+}
+
+void write_file_stream_char(Stream stream, Character c_object)
+{
+    fputc(CHARACTER(c_object), FILE_STREAM(theSTREAM(stream)));
+}
+
+void write_file_stream_integer(Stream stream, Integer i_object)
+{
+    fprintf(FILE_STREAM(theSTREAM(stream)), "%d", INTEGER(i_object));
+}
+
+void write_stream_char(Stream str, Character c) /* 我可能要写很多个版本的write_stream_*函数了，因为要对付许多不同的类型。 */
+{
+    switch (theSTREAM(str)->type) {
+    case FILE_STREAM:
+        return write_file_stream_char(str, c);
+        break;
+    default :
+        fprintf(stderr, "Unknown stream type %d.\n", theSTREAM(str)->type);
+        exit(1);
+    }
+}
+
+void write_stream_string(Stream stream, String string)
+{
+    fprintf(FILE_STREAM(theSTREAM(stream)), "%s", STRING(string));
 }
