0e0a4aff15f2f3b24124def125338edcae2bfa55
LiuTao <mat.liutos@gmail.com>
2012-11-02 22:16:30 +0800

    为了适应tagged pointer的使用而作出一定的修改

--- src/print_sexp.c
+++ src/print_sexp.c
@@ -1,48 +1,41 @@
 /*
  * print_sexp.c
  *
- * Prints an inner structures of a S-expression to screen.
+ * Prints an inner structures of a S-expression to screen. Depends on the low-
+ * level operations implemented in stream module.
  *
  * Copyright (C) 2012-10-03 liutos
  */
 #include "types.h"
 #include "atom_proc.h"
 #include "stream.h"
+#include "object.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
-extern void print_cons(LispObject);
+void print_sexp_notln(LispObject, Stream);
+void print_cons(LispObject, Stream);
 
-void print_sexp_notln(LispObject);
-void print_cons(LispObject);
-
-void print_atom(Atom atom)
+void print_atom(Atom atom, Stream output)
 {
     switch (TYPE(atom)) {
     case SYMBOL:
-        {
-            int i;
-
-            for (i = 0; SYMBOL_NAME(atom)[i] != '\0'; i++)
-                /* printf("%c", toupper(SYMBOL_NAME(atom)[i])); */
-                write_stream_char(standard_output,
-                                  make_char(toupper(SYMBOL_NAME(atom)[i])));
-        }
+        /* print_symbol(theSYMBOL(atom), stream); */
+        printf("%s", theSYMBOL(atom)->symbol_name);
 	break;
     case INTEGER:
-	/* printf("%d", INTEGER(atom)); */
-        write_file_stream_integer(standard_output, atom);
+        write_file_stream_integer(output, atom);
 	break;
     case FUNCTION:
 	if (TRUE == FUNC_FLAG(atom))
 	    printf("#<FUNCTION C %p>", atom);
 	else {
 	    printf("#<FUNCTION I ");
-	    print_cons(PARAMETERS(atom));
+	    print_cons(PARAMETERS(atom), output);
 	    putchar(' ');
-	    print_sexp_notln(EXPRESSION(atom));
+	    print_sexp_notln(EXPRESSION(atom), output);
 	    putchar(' ');
 	    printf("%p>", atom);
 	}
@@ -51,7 +44,7 @@ void print_atom(Atom atom)
         printf("\"%s\"", STRING(atom));
         break;
     case CHARACTER:
-        printf("#\\%c", CHARACTER(atom));
+        write_stream_char(standard_output, atom);
         break;
     case STREAM:
         printf("#<STREAM %p>", atom);
@@ -62,47 +55,48 @@ void print_atom(Atom atom)
     }
 }
 
-void print_cons_core(LispObject cons)
+void print_cons_core(LispObject cons, Stream output)
 {
     while (!is_tail(cons)) {
         /* The variable `cons' here would never be the lt_void
            so it's safe to use the macro CAR instead of SCAR. */
-	if (CAR(cons)->type != CONS)
-	    print_atom(CAR(cons));
-	else
-	    print_cons(CAR(cons));
+	if (CONS_P(CAR(cons)))
+            print_cons(CAR(cons), output);
+        else
+            print_atom(CAR(cons), output);
 	cons = CDR(cons);
 	if (is_atom_object(cons)) {
 	    if (cons != lt_nil) {
 		printf(" . ");
-		print_atom(cons);
+		print_atom(cons, output);
 	    }
 	} else
             putchar(' ');
     }
 }
 
-void print_cons(LispObject cons)
+void print_cons(LispObject cons, Stream output)
 {
     putchar('(');
-    print_cons_core(cons);
+    print_cons_core(cons, output);
     putchar(')');
 }
 
-void print_sexp_notln(LispObject object)
+void print_sexp_notln(LispObject object, Stream output)
 {
-    if (CONS == object->type)
-	print_cons(object);
+    if (CONS_P(object))
+	print_cons(object, output);
     else
-	print_atom(object);
+	print_atom(object, output);
 }
 
-void print_sexp(LispObject object)
+/* Print the representation of OBJECT to OUTPUT. */
+void print_sexp(LispObject object, Stream output)
 {
     if (NULL == object) {
         printf("; No value\n");
         return;
     }
-    print_sexp_notln(object);
+    print_sexp_notln(object, output);
     putchar('\n');
 }
