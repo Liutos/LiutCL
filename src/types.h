52f951ca842a068e8c903c907ea67d43e9628265
LiuTao <mat.liutos@gmail.com>
2012-11-02 22:18:06 +0800

    利用tagged pointer，重新定义了一些数据类型，以及定义了一些新的宏。

--- src/types.h
+++ src/types.h
@@ -33,14 +33,20 @@ typedef int BOOL;
 #define TRUE 1
 #define FALSE 0
 
+/* Cons definition */
+typedef struct cons_t {
+    LispObject car;
+    LispObject cdr;
+} *cons_t;
+
 /* Function definition. */
 typedef struct function_t {
     BOOL is_c_func;
     union {
 	primitive_t prim;
 	struct {
-	    struct LispObject *parms;
-	    struct LispObject *expr;
+	    LispObject parms;
+	    LispObject expr;
 	    struct Environment *env; /* Lexical Bindings */
 	    struct Environment *denv; /* Dynamic Bindings */
             struct block_environment *block_env; /* Block Environment */
@@ -50,13 +56,11 @@ typedef struct function_t {
 } *function_t;                  /* The inner representation of the
                                    function type in Lisp level. */
 /* Symbol definition */
-struct symbol_t;
-typedef struct symbol_t *symbol_t;
-struct symbol_t {
+typedef struct symbol_t {
     char *symbol_name;
     LispObject value_cell;
     LispObject function_cell;
-};
+} *symbol_t;
 
 /* Stream definition */
 typedef enum {
@@ -65,18 +69,13 @@ typedef enum {
     BYTE_STREAM,
 } STREAM_TYPE;
 
-struct stream_t;
-typedef struct stream_t *stream_t;
-typedef stream_t file_stream;
-typedef stream_t char_stream;
-typedef stream_t byte_stream;
-struct stream_t {
+typedef struct stream_t {
     STREAM_TYPE type;
     union {
         FILE *file;
         char *string;
     } u;
-};
+} *stream_t;
 
 /* Vector definition */
 struct vector_t;
@@ -86,29 +85,39 @@ struct vector_t {
     LispObject *content;        /* A C array. */
 };
 
-/* Lisp Object definition */
+/* Lisp Object definition. Maybe this could be called as `tagged union' */
 struct LispObject {
     LispType type;
     union {
-	struct symbol_t *symbol;
-	int integer;
 	function_t fun;
         char *string;
         struct stream_t *stream;
-	struct {
-	    LispObject car;
-	    LispObject cdr;
-	} s;
-        char c;                 /* The character */
         struct vector_t *vector;
     } u;
 };
 
 /* Object */
-#define TYPE(x) ((x)->type)
+#define TYPE(x) (enum_type_of(x))
+#define TYPE_TAG(x) ((int)(x) & 7) /* Used for tagged pointer. */
+#define UNTAG(x) ((int)(x) & ~7)
+
+#define POINTER_TAG 0
+#define INTEGER_TAG 1
+#define CONS_TAG 2
+#define SYMBOL_TAG 3
+#define CHARACTER_TAG 4
+
+#define POINTER_P(x) (TYPE_TAG(x) == POINTER_TAG)
+#define INTEGER_P(x) (TYPE_TAG(x) == INTEGER_TAG)
+#define CONS_P(x) (TYPE_TAG(x) == CONS_TAG)
+#define SYMBOL_P(x) (TYPE_TAG(x) == SYMBOL_TAG)
+#define CHARACTER_P(x) (TYPE_TAG(x) == CHARACTER_TAG)
 /* Cons Object */
-#define CAR(x) ((x)->u.s.car)
-#define CDR(x) ((x)->u.s.cdr)
+#define MAKE_CONS(x) ((LispObject)((int)(x) | CONS_TAG))
+#define GET_CONS(x) ((cons_t)UNTAG(x))
+
+#define CAR(x) (GET_CONS(x)->car)
+#define CDR(x) (GET_CONS(x)->cdr)
 #define SCAR(x) safe_car(x)     /* A safe version of macro CAR */
 #define SCDR(x) safe_cdr(x)     /* A safe version of macro CDR */
 #define FIRST(x) CAR(x)
@@ -116,8 +125,11 @@ struct LispObject {
 #define CDDR(x) SCDR(SCDR(x))
 #define THIRD(x) SCAR(CDDR(x))
 #define FOURTH(x) SCAR(SCDR(CDDR(x)))
+
 /* Integer Object */
-#define INTEGER(x) ((x)->u.integer)
+#define INTEGER(x) ((int)(x) >> 3)
+#define MAKE_INTEGER(x) ((LispObject)(((int)(x) << 3) | INTEGER_TAG))
+
 /* Function Object */
 #define FUNCTION(x) ((x)->u.fun)
 #define LOCAL_ENV(x) (FUNCTION(x)->env)
@@ -128,21 +140,26 @@ struct LispObject {
 #define EXPRESSION(x) (FUNCTION(x)->expr)
 #define PARAMETERS(x) (FUNCTION(x)->parms)
 #define FENV(x) (FUNCTION(x)->fenv)
+
 /* Symbol Object */
-#define theSYMBOL(x) ((x)->u.symbol)
+#define MAKE_SYMBOL(x) ((LispObject)((int)(x) | SYMBOL_TAG))
+#define theSYMBOL(x) ((symbol_t)UNTAG(x))
 #define SYMBOL_NAME(x) (theSYMBOL(x)->symbol_name)
+
 /* String Object */
 #define STRING(x) ((x)->u.string)
+
 /* Character Object */
-#define CHARACTER(x) ((x)->u.c)
+#define MAKE_CHARACTER(x) ((LispObject)(((int)(x) << 3) | CHARACTER_TAG))
+#define CHARACTER(x) ((int)(x) >> 3)
+
 /* Stream Object */
 #define theSTREAM(x) ((x)->u.stream)
 #define FILE_STREAM(x) ((x)->u.file)
+
 /* Vector Object */
 #define theVector(x) ((x)->u.vector)
 
 #define PHEAD(func_name) LispObject func_name(Cons args)
-
-extern BOOL is_debug_mode;      /* Not used yet */
 
 #endif
