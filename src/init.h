a2790b9075886e8dceb1d3c04b7b382ffd48390c
LiuTao <mat.liutos@gmail.com>
2012-11-05 18:36:28 +0800

    新增文件，init.c文件的函数声明。

---
+++ src/init.h
@@ -0,0 +1,10 @@
+#ifndef INIT_H
+#define INIT_H
+
+#include "env_types.h"
+
+extern Environment init_primitives(Environment);
+extern Environment init_variables(Environment);
+extern void init_special_operators(void);
+
+#endif
