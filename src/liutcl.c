6dcf841d0ece7aac1fc865a563bba2bcac4f094c
LiuTao <mat.liutos@gmail.com>
2012-11-05 18:32:33 +0800

    无关紧要的修改

--- src/liutcl.c
+++ src/liutcl.c
@@ -1,43 +1,42 @@
 /*
  * liutcl.c
  *
- * The implementation of the toplevel interface.
+ * 
  *
  * Copyright (C) 2012-10-15 liutos mat.liutos@gmail.com
  */
-#include "atom_proc.h"
-#include "environment.h"
-#include "types.h"
-#include "parse_sexp.h"
-#include "read_sexp.h"
-#include "eval_sexp.h"
-#include "print_sexp.h"
-#include "env_types.h"
-#include "stream.h"
-
 #include <stdio.h>
+
+#include "env_types.h"
+#include "environment.h"
+#include "eval_sexp.h"
+#include "init.h"
+#include "parse_sexp.h"
+#include "print_sexp.h"
+#include "read_sexp.h"
+#include "stream.h"
+#include "types.h"
 
 int main(int argc, char *argv[])
 {
-    Environment lexical_env;    /* Lexical environment */
-    Environment dynamic_env;    /* Dynamic environment */
-    Environment fun_env;        /* Function definition environment. */
-    char *expr;
-    LispObject sexp;
-    BlockEnvironment block_env; /* The block environment. */
+    Environment lexical_env;
+    Environment fdefinition_env;
+    char *input;
+    LispObject sexp, result;
+    BlockEnvironment block_env;
 
-    init_symbol_table();
-    lexical_env = new_env();
-    lexical_env = init_variables(lexical_env);
-    dynamic_env = new_env();
-    /* dynamic_env = init_primitives(dynamic_env); */
-    fun_env = new_env();
-    fun_env = init_primitives(fun_env);
+    init_special_operators();
+    global_dynamic_env = make_empty_env();
     block_env = NULL;
+    fdefinition_env = make_empty_env();
+    fdefinition_env = init_primitives(fdefinition_env);
+    lexical_env = make_empty_env();
+    lexical_env = init_variables(lexical_env);
     do {
-        expr = read_sexp(stdin);
-        sexp = parse_sexp(expr);
-        print_sexp(eval_sexp(sexp, lexical_env, dynamic_env, block_env, fun_env), standard_output);
+        input = read_sexp(stdin);
+        sexp = parse_sexp(input);
+        result = eval_sexp(sexp, lexical_env, global_dynamic_env, block_env, fdefinition_env);
+        print_object(result, standard_output);
     } while (1);
 
     return 0;
