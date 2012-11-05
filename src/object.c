79e1c5430ed223cde0589089701170ef5f22aeb9
LiuTao <mat.liutos@gmail.com>
2012-11-05 18:34:34 +0800

    修改了enum_type_of中的代码排列顺序

--- src/object.c
+++ src/object.c
@@ -1,36 +1,26 @@
 /*
  * object.c
  *
- * Some operations common among all type of objects.
+ * 
  *
  * Copyright (C) 2012-10-05 liutos
  */
-#include "types.h"
-
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-LispObject new_object(void)
+#include "types.h"
+
+LispObject g_unbound;
+
+inline LispObject make_object(void)
 {
-    return malloc(sizeof(struct LispObject));
-}
-
-symbol_t make_C_symbol(char *symbol_name)
-{
-    symbol_t symbol = malloc(sizeof(struct symbol_t));
-
-    symbol->symbol_name = symbol_name;
-    symbol->value_cell = NULL;
-    symbol->function_cell = NULL;
-
-    return symbol;
+    return malloc(sizeof(struct lisp_object_t));
 }
 
 stream_t make_C_file_stream(FILE *fp)
 {
     stream_t stream = malloc(sizeof(struct stream_t));
-
     stream->type = FILE_STREAM;
     stream->u.file = fp;
 
@@ -40,18 +30,19 @@ stream_t make_C_file_stream(FILE *fp)
 stream_t make_C_string_stream(char *string)
 {
     stream_t stream = malloc(sizeof(struct stream_t));
-
     stream->type = CHARACTER_STREAM;
-    stream->u.string = strdup(string);
+    stream->u.s.string = strdup(string);
 
     return stream;
 }
 
 LispType enum_type_of(LispObject object)
 {
-    if (INTEGER_P(object)) return INTEGER;
-    if (SYMBOL_P(object)) return SYMBOL;
-    if (CHARACTER_P(object)) return CHARACTER;
+    if (CHAR_P(object)) return CHARACTER;
     if (CONS_P(object)) return CONS;
+    if (FIXNUM_P(object)) return FIXNUM;
+    if (FUNCTION_P(object)) return FUNCTION;
+    if (STRING_P(object)) return STRING;
+    if (SYMBOL_P(object)) return SYMBOL;
     else return object->type;
 }
