4068315e318df535f7055aadb74600b38454dbff
LiuTao <mat.liutos@gmail.com>
2012-11-05 18:29:13 +0800

    *** empty log message ***

--- src/cons.c
+++ src/cons.c
@@ -1,44 +1,49 @@
 /*
  * cons.c
  *
- * Operators on objects of type Cons
+ * 
  *
  * Copyright (C) 2012-10-04 liutos
  */
-#include <stdlib.h>
 #include <assert.h>
+#include <stdlib.h>
 
-#include "types.h"
-#include "object.h"
 #include "atom_proc.h"
+#include "object.h"
+#include "types.h"
 
-/* Tagged Pointer version */
-Cons make_cons_cell(LispObject car, LispObject cdr)
+Cons make_cons(LispObject car, LispObject cdr)
 {
     cons_t cons = malloc(sizeof(struct cons_t));
-
     cons->car = car;
     cons->cdr = cdr;
 
-    return MAKE_CONS(cons);
+    return TO_CONS(cons);
 }
 
-void free_cons_cell(Cons cons)
+void free_cons(Cons cons)
 {
-    assert(lt_nil == cons || CONS_P(cons));
-    free(GET_CONS(cons));
+    assert(CONS_P(cons));
+
+    free(theCONS(cons));
 }
 
 LispObject safe_car(LispObject obj)
 {
     assert(lt_nil == obj || CONS_P(obj));
 
-    return lt_nil == obj ? lt_nil: GET_CONS(obj)->car;
+    return lt_nil == obj ? lt_nil: theCONS(obj)->car;
 }
 
 LispObject safe_cdr(LispObject obj)
 {
     assert(lt_nil == obj || CONS_P(obj));
 
-    return lt_nil == obj ? lt_nil: GET_CONS(obj)->cdr;
+    return lt_nil == obj ? lt_nil: theCONS(obj)->cdr;
+}
+
+unsigned int cons_length(Cons cons)
+{
+    if (lt_nil == cons) return 0;
+    else return 1 + cons_length(CDR(cons));
 }
