c888dede9d84574bc588c0185f52e1abf89c8cd0
LiuTao <mat.liutos@gmail.com>
2012-11-05 18:32:48 +0800

    无关要紧的修改

--- src/atom_proc.h
+++ src/atom_proc.h
@@ -1,28 +1,33 @@
 #ifndef ATOM_PROC_H
 #define ATOM_PROC_H
 
-#include "types.h"
 #include "env_types.h"
+#include "types.h"
 
-extern Symbol make_symbol(char *);
-extern Symbol ensure_symbol_exists(char *);
-extern Function make_c_fun_object(primitive_t);
-extern BOOL is_true_obj(LispObject);
-extern Function make_i_fun_object(Cons, LispObject, Environment, Environment, BlockEnvironment);
 extern BOOL is_atom_object(LispObject);
-extern BOOL is_tail(LispObject);
 extern BOOL is_symbol(LispObject);
+extern BOOL is_tail(LispObject);
+extern BOOL is_true_obj(LispObject);
 extern Character make_char(char);
+extern Fixnum make_fixnum(int);
+extern Function make_Lisp_function(Cons, LispObject, Environment, Environment, BlockEnvironment, Environment);
+extern Function make_C_function(primitive_t, int);
+extern String make_string(char *);
 
 extern Symbol lt_nil;
 extern Symbol lt_t;
-extern Symbol lt_quote, lt_if, lt_begin, lt_set, lt_lambda;
 
-extern Symbol lt_dset;
-extern Symbol lt_dynamic;
-extern Symbol lt_catch;
-extern Symbol lt_throw;
 extern Symbol lt_block;
+extern Symbol lt_catch;
+extern Symbol lt_defvar;
+extern Symbol lt_fset;
+extern Symbol lt_function;
+extern Symbol lt_if;
+extern Symbol lt_lambda;
+extern Symbol lt_progn;
+extern Symbol lt_quote;
 extern Symbol lt_return_from;
+extern Symbol lt_set;
+extern Symbol lt_throw;
 
 #endif
