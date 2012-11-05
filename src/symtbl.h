e39a6f265d320811b47edbc27069376a630e35aa
LiuTao <mat.liutos@gmail.com>
2012-11-05 18:33:44 +0800

    删掉无聊的注释

--- src/symtbl.h
+++ src/symtbl.h
@@ -3,22 +3,14 @@
 
 #include "types.h"
 
-/* typedef struct StrSymMap { */
-/*     char *symbol_name; */
-/*     struct LispObject *symbol; */
-/*     struct StrSymMap *next; */
-/* } *SymbolTable; */
-
-/* Implements the symbol table as a binary search tree.
-   AVL tree or red-black tree is better. */
-
-/* AVL tree version */
-typedef struct StrSymMapNode {
-    struct LispObject *symbol;
-    struct StrSymMapNode *left, *right, *parent;
-    int height;                 /* Height of the tree. */
-    int bf;                     /* balance factor */
-} *SymbolTable, *StrSymMapNode;
-/* Please give a Trie version below. */
+/* AVL tree implementation */
+typedef struct StrSymMapNode *StrSymMapNode;
+typedef struct StrSymMapNode *SymbolTable;
+struct StrSymMapNode {
+    LispObject symbol;
+    StrSymMapNode left, parent, right;
+    int bf;                     /* Balance factor */
+    int height;                 /* Height of the tree */
+};
 
 #endif
