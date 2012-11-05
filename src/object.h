0fa7751dc61c1ad17c30081406ea2f5f5ac8a201
LiuTao <mat.liutos@gmail.com>
2012-11-05 18:33:32 +0800

    无关要紧的修改

--- src/object.h
+++ src/object.h
@@ -1,14 +1,15 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
-#include "types.h"
-
 #include <stdio.h>
 
-extern LispObject new_object(void);
-extern symbol_t make_C_symbol(char *);
+#include "types.h"
+
+extern LispObject make_object(void);
+extern LispType enum_type_of(LispObject);
 extern stream_t make_C_file_stream(FILE *);
 extern stream_t make_C_string_stream(char *);
-extern LispType enum_type_of(LispObject);
+
+extern LispObject g_unbound;
 
 #endif
