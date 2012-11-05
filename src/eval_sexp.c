c06e909ba39a80cc040d4b9af8b31862aae513ad
LiuTao <mat.liutos@gmail.com>
2012-11-05 18:29:00 +0800

    一、重排了大量的函数定义 二、将特殊操作符的调用处理从eval_cons中剥离出来，定义is_special_operators用于判断一个符号是否对应一个特殊操作符。 三、将一些特殊操作符的处理代码提炼为函数从eval_spec中剥离出来 四、eprogn改名为eval_progn 五、增加check_arity函数，用于检查参数个数是否与声明的一致。

--- src/eval_sexp.c
+++ src/eval_sexp.c
@@ -1,250 +1,303 @@
 /*
  * eval_sexp.c
  *
- * Evaluator for S-expressions.
+ * Evaluator of S-expression.
  *
  * Copyright (C) 2012-10-04 liutos
  */
-#include "types.h"
-#include "environment.h"
-#include "atom_proc.h"
-#include "primitives.h"
-#include "print_sexp.h"
-#include "cons.h"
-#include "env_types.h"
-#include "stream.h"
-
+#include <setjmp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <setjmp.h>
 #include <string.h>
 
+#include "atom_proc.h"
+#include "cons.h"
+#include "env_types.h"
+#include "environment.h"
+#include "primitives.h"
+#include "print_sexp.h"
+#include "stream.h"
+#include "types.h"
+
+#define CALL_EVAL(eval_fn, arg) eval_fn(arg, lenv, denv, benv, fenv)
+#define DEFEVAL(name, hd)                       \
+    LispObject name(LispObject hd,              \
+                    Environment lenv,           \
+                    Environment denv,           \
+                    BlockEnvironment benv,      \
+                    Environment fenv)
 #define eq(x, y) (x == y)
-#define DEFEVAL(name, hd) LispObject name(LispObject hd, Environment env, Environment denv, BlockEnvironment block_env, Environment fenv)
 
-/* LispObject eval_sexp(LispObject, Environment, Environment, BlockEnvironment, Environment); */
-/* LispObject eval_cons(Cons, Environment, Environment, BlockEnvironment, Environment); */
-DEFEVAL(eval_sexp, _);
+DEFEVAL(eval_progn, _);
 DEFEVAL(eval_cons, _);
+DEFEVAL(eval_sexp, _);
 
-jmp_buf context;
+jmp_buf escape;
 
-LispObject eval_atom(Atom exp, Environment env)
+inline LispObject invoke_C_function(Function C_function, Cons args)
 {
-    LispObject tmp;
+    return PRIMITIVE(C_function)(args);
+}
 
-    if (is_symbol(exp)) {
-	tmp = get_value(exp, env);
+LispObject invoke_Lisp_function(Function Lisp_function, Cons args, Environment denv)
+/* Invokes a Lisp function means evaluating the function's body in a extended
+   environment. */
+{
+    BlockEnvironment benv = BLOCK_ENV(Lisp_function);
+    Environment fenv = FDEFINITION_ENV(Lisp_function);
+    Environment lenv =
+        make_new_env(PARAMETERS(Lisp_function),
+                     args,
+                     LEXICAL_ENV(Lisp_function));
+    
+    return CALL_EVAL(eval_progn, EXPRESSION(Lisp_function));
+}
+
+LispObject invoke_function(Function function, Cons args, Environment denv)
+{
+    if (TRUE == FUNCTION_CFLAG(function))
+	return invoke_C_function(function, args);
+    else
+	return invoke_Lisp_function(function, args, denv);
+}
+
+BOOL is_special_operator(LispObject op)
+{
+    Symbol lt_specs[] = {
+        lt_block,
+        lt_catch,
+        lt_defvar,
+        lt_function,
+        lt_fset,
+        lt_if,
+        lt_lambda,
+        lt_progn,
+        lt_quote,
+        lt_return_from,
+        lt_set,
+        lt_throw,
+    };
+    int length = sizeof(lt_specs) / sizeof(Symbol);
+    int i;
+    for (i = 0; i < length; ++i)
+        if (eq(op, lt_specs[i]))
+            return TRUE;
+
+    return FALSE;
+}
+
+DEFEVAL(eval_block, argv)
+{
+    jmp_buf context;
+    int val;
+    Symbol name = FIRST(argv);
+    val = setjmp(context);
+    if (0 == val)
+        return eval_progn(CDR(argv), lenv, denv,
+                          make_block_env(name, context, benv), fenv);
+    else
+        return (LispObject)val;
+}
+
+DEFEVAL(eval_catch, argv)
+{
+    jmp_buf context;
+    int val;
+    LispObject tag = CALL_EVAL(eval_sexp, FIRST(argv));
+    /* Simulates the dynamic scope by twice application of memcpy */
+    memcpy(context, escape, sizeof(jmp_buf));
+    val = setjmp(escape);
+    if (0 == val) {
+        LispObject value = CALL_EVAL(eval_progn, CDR(argv));
+        memcpy(escape, context, sizeof(jmp_buf));
+
+        return value;
+    } else {                    /* Returns from a invokation of THROW */
+        Cons cons = (Cons)val;
+        memcpy(escape, context, sizeof(jmp_buf));
+        if (eq(CAR(cons), tag))
+            return CDR(cons);
+        else
+            longjmp(escape, val);
+    }
+}
+
+DEFEVAL(eval_defvar, argv)
+{
+    Symbol name = FIRST(argv);
+    if (NULL == get_value(name, global_dynamic_env))
+        global_dynamic_env =
+            extend_env(name,
+                       CALL_EVAL(eval_sexp, SECOND(argv)),
+                       global_dynamic_env);
+
+    return name;
+}
+
+DEFEVAL(eval_function, arg)
+{
+    return get_value(arg, fenv);
+}
+
+DEFEVAL(eval_progn, exps)
+{
+    if (is_tail(exps))
+	return lt_nil;
+    if (is_tail(CDR(exps)))
+	return CALL_EVAL(eval_sexp, CAR(exps));
+    while (!is_tail(exps)) {
+	if (is_tail(CDR(exps)))
+	    break;
+	CALL_EVAL(eval_sexp, CAR(exps));
+	exps = CDR(exps);
+    }
+
+    return CALL_EVAL(eval_sexp, CAR(exps));
+}
+
+DEFEVAL(eval_return_from, argv)
+{
+    Symbol name = FIRST(argv);
+    LispObject result = CALL_EVAL(eval_sexp, SECOND(argv));
+    while (benv != NULL) {
+        if (benv->name == name)
+            longjmp(benv->context, (int)result);
+        benv = benv->prev;
+    }
+    write_format(standard_error, "No such a block contains name %!\n", name);
+    exit(1);
+}
+
+DEFEVAL(eval_throw, argv)
+{
+    LispObject tag = CALL_EVAL(eval_sexp, FIRST(argv));
+    LispObject result = CALL_EVAL(eval_sexp, SECOND(argv));
+    longjmp(escape, (int)make_cons(tag, result));
+}
+
+DEFEVAL(eval_spec, exp)
+{
+    Cons argv = CDR(exp);
+    LispObject op = FIRST(exp);
+
+    if (eq(op, lt_block))
+        return CALL_EVAL(eval_block, argv);
+    if (eq(op, lt_catch))
+        return CALL_EVAL(eval_catch, argv);
+    if (eq(op, lt_defvar))
+        return CALL_EVAL(eval_defvar, argv);
+    if (eq(op, lt_fset)) {
+        extend_env(FIRST(argv), CALL_EVAL(eval_sexp, SECOND(argv)), fenv);
+
+        return lt_nil;
+    }
+    if (eq(op, lt_function))
+        return CALL_EVAL(eval_function, FIRST(argv));
+    if (eq(op, lt_if)) {
+	if (is_true_obj(CALL_EVAL(eval_sexp, FIRST(argv))))
+            return CALL_EVAL(eval_sexp, SECOND(argv));
+	else
+            return CALL_EVAL(eval_sexp, THIRD(argv));
+    }
+    if (eq(op, lt_lambda))
+	return make_Lisp_function(CAR(argv), CDR(argv), lenv, denv, benv, fenv);
+    if (eq(op, lt_progn))
+        return CALL_EVAL(eval_progn, argv);
+    if (eq(op, lt_quote))
+        return FIRST(argv);
+    if (eq(op, lt_return_from))
+        return CALL_EVAL(eval_return_from, argv);
+    if (eq(op, lt_set)) {
+        extend_env(FIRST(argv), CALL_EVAL(eval_sexp, SECOND(argv)), lenv);
+
+        return lt_nil;
+    }
+    if (eq(op, lt_throw))
+        return CALL_EVAL(eval_throw, argv);
+    write_string(standard_error, TO_STRING("WTF!\n"));
+    exit(1);
+}
+
+/* Argument `parms' must be a proper list. */
+int check_arity(int arity, Cons parms)
+{
+    if (0 == arity && eq(lt_nil, parms)) return 0;
+    if (0 == arity) return cons_length(parms);
+    if (eq(lt_nil, parms)) return arity;
+    return check_arity(arity - 1, CDR(parms));
+}
+
+DEFEVAL(eval_operator, op)
+{
+    if (SYMBOL_P(op)) {
+	LispObject tmp = get_value(op, fenv);
 	if (tmp != NULL)
 	    return tmp;
 	else {
-	    fprintf(stderr, "No binding of symbol %s.\n", SYMBOL_NAME(exp));
+            write_format(standard_error, "No binding of symbol %!\n", op);
+	    exit(1);
+	}
+    } else
+	return CALL_EVAL(eval_cons, op);
+}
+
+DEFEVAL(eval_args, args)
+{
+    Cons head, pre, cur;
+
+    pre = head = make_cons(lt_nil, lt_nil);
+    while (!is_tail(args)) {
+        cur = make_cons(CALL_EVAL(eval_sexp, CAR(args)), lt_nil);
+        _CDR(pre) = cur;
+        pre = cur;
+        args = CDR(args);
+    }
+
+    return CDR(head);
+}
+
+DEFEVAL(eval_cons, exps)
+{
+    Cons args = CDR(exps);
+    LispObject op = FIRST(exps);
+    if (is_special_operator(op))
+        return CALL_EVAL(eval_spec, exps);
+    else {
+        Function fn = CALL_EVAL(eval_operator, op);
+        int result = check_arity(ARITY(fn), args);
+        if (result != 0) {
+            write_format(standard_error, "Wrong number of args\n");
+            exit(1);
+        } else {
+            args = CALL_EVAL(eval_args, args);
+
+            return invoke_function(fn, args, denv);
+        }
+    }
+}
+
+LispObject eval_atom(Atom exp, Environment lenv, Environment denv)
+{
+    if (SYMBOL_P(exp)) {
+	LispObject tmp = get_value(exp, lenv);
+	if (tmp != NULL)
+	    return tmp;
+        else if ((tmp = get_value(exp, denv)) != NULL)
+            return tmp;
+	else {
+            write_format(standard_error, "No binding of symbol %!\n", exp);
 	    exit(0);
 	}
     } else
 	return exp;
 }
 
-LispObject invoke_c_fun(Function cfunc, Cons args)
-{
-    return PRIMITIVE(cfunc)(args);
-}
-
-LispObject invoke_i_fun(Function ifunc, Cons args)
-/* Invokes a closure means evaluating the function body's expressions in
-   a locally newly extended environment. */
-{
-    Environment env;
-
-    /* This is the `newly extended environment'. */
-    /* For processing correctly, there must be a function designed for
-       interpreted function invokation because at that case, the outer
-       environment exists when the closure was created, its content
-       shouldn't be modified by the function application. */
-    env = new_binding_env(PARAMETERS(ifunc), args, LOCAL_ENV(ifunc));
-
-    return eval_sexp(EXPRESSION(ifunc), env, FUNCTION(ifunc)->denv, BLOCK_ENV(ifunc), FENV(ifunc));
-}
-
-LispObject invoke_function(Function func, Cons args)
-{
-    if (TRUE == FUNC_FLAG(func))
-	return invoke_c_fun(func, args);
-    else
-	return invoke_i_fun(func, args);
-}
-
-DEFEVAL(eprogn, exps)
-/* This function name used in _Lisp in Small Pieces_. */
-{
-    if (is_tail(exps))
-	return lt_nil;
-    /* The if statement above ensures that the parameter `exps' used
-       below would never be the lt_void, the empty list in the Lisp
-       level. */
-    if (is_tail(CDR(exps)))
-	return eval_sexp(CAR(exps), env, denv, block_env, fenv);
-    while (!is_tail(exps)) {
-	if (is_tail(CDR(exps)))
-	    break;
-	eval_sexp(CAR(exps), env, denv, block_env, fenv);
-	exps = CDR(exps);
-    }
-
-    return eval_sexp(CAR(exps), env, denv, block_env, fenv);
-}
-
-DEFEVAL(eval_args, args)
-{
-    if (!is_tail(args))
-	return make_cons_cell(eval_sexp(CAR(args), env, denv, block_env, fenv),
-			      eval_args(CDR(args), env, denv, block_env, fenv));
-    else
-	return lt_nil;
-}
-
-DEFEVAL(eval_operator, op)
-{
-    if (is_symbol(op)) {
-	LispObject tmp;
-
-	tmp = get_value(op, fenv);
-	if (tmp != NULL)
-	    return tmp;
-	else {
-	    fprintf(stderr, "No binding of symbol %s.\n", SYMBOL_NAME(op));
-	    exit(1);
-	}
-    } else
-	return eval_cons(op, env, denv, block_env, fenv);
-}
-
-DEFEVAL(eval_catch, argv)
-{
-    jmp_buf tmp_context;
-    int val;
-    Symbol label;
-
-    label = eval_sexp(FIRST(argv), env, denv, block_env, fenv);
-    memcpy(tmp_context, context, sizeof(jmp_buf));
-    val = setjmp(context);
-    if (0 == val) {
-        LispObject val = eprogn(SCDR(argv), env, denv, block_env, fenv);
-        if (tmp_context != NULL)
-            memcpy(context, tmp_context, sizeof(jmp_buf));
-
-        return val;
-    } else {
-        SymValMap tmp_map = (SymValMap)val;
-
-        memcpy(context, tmp_context, sizeof(jmp_buf));
-        if (tmp_map->symbol == label)
-            return tmp_map->value;
-        else
-            longjmp(context, (int)tmp_map);
-    }
-}
-
-DEFEVAL(eval_cons, exps)
-/* This function should handles only the Lisp objects of type Cons, 
-   not included the symbol `nil'. */
-{
-    Function func;
-    Cons argv;
-    LispObject op;
-
- INTERP:                        /* The support of tail-recursion optimization */
-    argv = SCDR(exps);
-    op = SCAR(exps);
-    /* Cases of special operators in Scheme */
-    if (eq(op, lt_quote))
-	return CAR(argv);
-    if (eq(op, lt_if)) {   /* 如果把if对应的操作给函数化了，那么就不能应用goto来实现尾递归优化了。 */
-	if (is_true_obj(eval_sexp(FIRST(argv), env, denv, block_env, fenv))) {
-	    exps = SECOND(argv); /* A try of tail-recursive optimization. */
-            goto INTERP;
-	} else {
-            exps = THIRD(argv);
-            goto INTERP;
-        }
-    }
-    if (eq(op, lt_begin))
-	return eprogn(argv, env, denv, block_env, fenv);
-    if (eq(op, lt_set)) {
-        extend_binding(SCAR(argv),
-                       eval_sexp(SCAR(SCDR(argv)), env, denv, block_env, fenv),
-                       env);
-
-        return lt_nil;
-    }
-
-    if (eq(op, lt_lambda)) /* LAMBDA */
-	return make_i_fun_object(SECOND(exps), THIRD(exps), env, denv, block_env);
-    /* Process the special operator added by myself. */
-    if (eq(op, lt_dset)) {
-	extend_binding(SECOND(exps),
-		       eval_sexp(THIRD(exps), env, denv, block_env, fenv),
-		       denv);
-
-	return lt_nil;
-    }
-    if (eq(op, lt_dynamic)) /* Symbol LT/DYNAMIC */
-	return eval_atom(eval_sexp(SECOND(exps), env, denv, block_env, fenv), denv);
-    if (eq(op, lt_catch)) /* Symbol LT/CATCH */
-        return eval_catch(argv, env, denv, block_env, fenv);
-    if (eq(op, lt_throw)) { /* Symbol LT/THROW */
-        Symbol label = eval_sexp(FIRST(argv), env, denv, block_env, fenv); /* 这里假设了第一个参数是要求值的，因为我一时忘了Common Lisp是否对其进行求值了。 */
-        LispObject value = eval_sexp(SECOND(argv), env, denv, block_env, fenv);
-        SymValMap map = make_single_map(label, value);
-
-        longjmp(context, (int)map);
-    }
-    if (eq(op, lt_block)) { /* Symbol LT/BLOCK */
-        jmp_buf context;
-        int val;
-        Symbol name = FIRST(argv);/* eval_sexp(FIRST(argv), env, denv, block_env). This argument should not be evaluated. */;
-
-        val = setjmp(context);
-        if (0 == val)
-            return eprogn(CDR(argv), env, denv, make_block_env(name, context, block_env), fenv);
-        else
-            return (LispObject)val;
-    }
-    if (eq(op, lt_return_from)) { /* Symbol LT/RETURN-FROM */
-        Symbol name = FIRST(argv);     /* Name should not be evaluated. */
-        LispObject value = eval_sexp(SECOND(argv), env, denv, block_env, fenv);
-
-        while (block_env != NULL) {
-            if (block_env->name == name)
-                longjmp(block_env->context, (int)value);
-            block_env = block_env->prev;
-        }
-        printf("No such a block environment contains name `");
-        print_atom(name, standard_output);
-        printf("'.\n");
-        exit(1);
-    }
-
-    /* Regular function evaluation. */
-    func = eval_operator(op, env, denv, block_env, fenv);
-
-    return invoke_function(func, eval_args(argv, env, denv, block_env, fenv));
-}
-
-/* exp : inner representation of input expression;
- * env : lexical environment;
- * denv : dynamic environment;
- * block_env : environment contains bindings of name and context.
- */
 DEFEVAL(eval_sexp, exp)
-/* Parameter 'denv' means dynamic environment. Currently the `denv' also
-   stores the function value in it but I think providing a specific
-   environmen named fenv would be better. */
 {
     if (NULL == exp)
         return NULL;
     if (CONS_P(exp))
-        return eval_cons(exp, env, denv, block_env, fenv);
+        return CALL_EVAL(eval_cons, exp);
     else
-        return eval_atom(exp, env);
+        return eval_atom(exp, lenv, denv);
 }
