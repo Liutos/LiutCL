b2b4a6fc08a78208600532103e40f1f1b6bb20d4
LiuTao <mat.liutos@gmail.com>
2012-11-05 18:25:41 +0800

    一、修改了CAR和CDR宏的定义，使他们取代原来的SCAR和SCDR。而原来的可以作为左值使用的CAR和CDR则改名为_CAR和_CDR。 二、去掉SCAR和SCDR，增加取哈希表整体大小的TABLE_SIZE宏。

--- src/types.h
+++ src/types.h
@@ -3,35 +3,9 @@
 
 #include <stdio.h>
 
-typedef enum {
-    CONS,                       /* Everything except CONS is of type ATOM. */
-    SYMBOL,
-    INTEGER,
-    FUNCTION,
-    STRING,
-    STREAM,
-    CHARACTER,
-    VECTOR,
-} LispType;
+#include "decls.h"
 
-struct LispObject;
-typedef struct LispObject *LispObject;
-/* Alias. For readbility. */
-typedef LispObject Cons;
-typedef LispObject Atom;
-typedef LispObject Symbol;
-typedef LispObject Boolean;
-typedef LispObject Function;
-typedef LispObject Stream;
-typedef LispObject Character;
-typedef LispObject Vector;
-typedef LispObject Integer;
-typedef LispObject String;      /* 只是为了快速试验一下向文件流的写入而设的 */
-typedef LispObject (*primitive_t)(LispObject);
-
-typedef int BOOL;
-#define TRUE 1
-#define FALSE 0
+enum bool_t { FALSE, TRUE};
 
 /* Cons definition */
 typedef struct cons_t {
@@ -39,28 +13,43 @@ typedef struct cons_t {
     LispObject cdr;
 } *cons_t;
 
-/* Function definition. */
+/* Function definition */
 typedef struct function_t {
-    BOOL is_c_func;
+    BOOL is_C_function;
+    int arity;
     union {
-	primitive_t prim;
+	primitive_t fptr;
 	struct {
-	    LispObject parms;
-	    LispObject expr;
-	    struct Environment *env; /* Lexical Bindings */
-	    struct Environment *denv; /* Dynamic Bindings */
-            struct block_environment *block_env; /* Block Environment */
-            struct Environment *fenv;            /* Function definitions. */
-	};
-    };
-} *function_t;                  /* The inner representation of the
-                                   function type in Lisp level. */
-/* Symbol definition */
-typedef struct symbol_t {
-    char *symbol_name;
-    LispObject value_cell;
-    LispObject function_cell;
-} *symbol_t;
+	    Cons parameters;
+	    LispObject body;
+	    Environment lexical_env;
+            BlockEnvironment block_env;
+            Environment fdefinition_env;
+	} s;
+    } u;
+} *function_t;
+
+/* Hash table definition */
+typedef struct table_entry_t *table_entry_t;
+struct table_entry_t {
+    LispObject key;
+    LispObject value;
+    table_entry_t next;
+};
+
+typedef struct hash_table_t {
+    table_entry_t *elements;
+    unsigned int (*hash_fn)(LispObject, unsigned int);
+    BOOL (*compare_fn)(LispObject, LispObject);
+    unsigned int size;
+    unsigned int count;
+} *hash_table_t;
+
+/* Package definition */
+typedef struct package_t {
+    char *name;
+    hash_table_t table;
+} *package_t;
 
 /* Stream definition */
 typedef enum {
@@ -73,93 +62,152 @@ typedef struct stream_t {
     STREAM_TYPE type;
     union {
         FILE *file;
-        char *string;
+        struct {
+            char *string;
+            int position;
+        } s;
     } u;
 } *stream_t;
 
-/* Vector definition */
-struct vector_t;
-typedef struct vector_t *vector_t;
-struct vector_t {
+/* String definition */
+typedef struct string_t {
+    char *content;
     unsigned int length;
-    LispObject *content;        /* A C array. */
-};
+} *string_t;
 
-/* Lisp Object definition. Maybe this could be called as `tagged union' */
-struct LispObject {
+/* Symbol definition */
+typedef struct symbol_t {
+    char *name;
+    LispObject value;
+    LispObject function;
+    LispObject package;
+} *symbol_t;
+
+/* Vector definition */
+typedef struct vector_t {
+    unsigned int length;
+    LispObject *content;
+} *vector_t;
+
+/* Lisp object definition */
+typedef enum {
+    CHARACTER,
+    CONS,
+    FUNCTION,
+    FIXNUM,
+    HASH_TABLE,
+    PACKAGE,
+    STREAM,
+    STRING,
+    SYMBOL,
+    VECTOR,
+} LispType;
+
+struct lisp_object_t {
     LispType type;
     union {
-	function_t fun;
-        char *string;
-        struct stream_t *stream;
-        struct vector_t *vector;
+        hash_table_t hash_table;
+        package_t package;
+        stream_t stream;
+        vector_t vector;
     } u;
 };
 
-/* Object */
+/* Type */
 #define TYPE(x) (enum_type_of(x))
-#define TYPE_TAG(x) ((int)(x) & 7) /* Used for tagged pointer. */
+#define TAGOF(x) ((int)(x) & 7)
 #define UNTAG(x) ((int)(x) & ~7)
 
 #define POINTER_TAG 0
-#define INTEGER_TAG 1
+#define FIXNUM_TAG 1
 #define CONS_TAG 2
 #define SYMBOL_TAG 3
 #define CHARACTER_TAG 4
+#define FUNCTION_TAG 5
+#define STRING_TAG 6
 
-#define POINTER_P(x) (TYPE_TAG(x) == POINTER_TAG)
-#define INTEGER_P(x) (TYPE_TAG(x) == INTEGER_TAG)
-#define CONS_P(x) (TYPE_TAG(x) == CONS_TAG)
-#define SYMBOL_P(x) (TYPE_TAG(x) == SYMBOL_TAG)
-#define CHARACTER_P(x) (TYPE_TAG(x) == CHARACTER_TAG)
-/* Cons Object */
-#define MAKE_CONS(x) ((LispObject)((int)(x) | CONS_TAG))
-#define GET_CONS(x) ((cons_t)UNTAG(x))
+#define POINTER_P(x) (TAGOF(x) == POINTER_TAG)
+#define thePOINTER(x) ((LispObject)UNTAG(x))
 
-#define CAR(x) (GET_CONS(x)->car)
-#define CDR(x) (GET_CONS(x)->cdr)
-#define SCAR(x) safe_car(x)     /* A safe version of macro CAR */
-#define SCDR(x) safe_cdr(x)     /* A safe version of macro CDR */
+/* Character */
+#define TO_CHAR(x) ((LispObject)(((int)(x) << 3) | CHARACTER_TAG))
+#define theCHAR(x) ((int)(x) >> 3)
+#define CHAR_P(x) (TAGOF(x) == CHARACTER_TAG)
+
+/* Cons */
+#define TO_CONS(x) ((LispObject)((int)(x) | CONS_TAG))
+#define theCONS(x) ((cons_t)UNTAG(x))
+#define CONS_P(x) (TAGOF(x) == CONS_TAG)
+
+#define _CAR(x) (theCONS(x)->car)
+#define _CDR(x) (theCONS(x)->cdr)
+#define CAR(x) safe_car(x)
+#define CDR(x) safe_cdr(x)
+#define CDDR(x) CDR(CDR(x))
 #define FIRST(x) CAR(x)
-#define SECOND(x) CAR(SCDR(x))
-#define CDDR(x) SCDR(SCDR(x))
-#define THIRD(x) SCAR(CDDR(x))
-#define FOURTH(x) SCAR(SCDR(CDDR(x)))
+#define SECOND(x) CAR(CDR(x))
+#define THIRD(x) CAR(CDDR(x))
+#define FOURTH(x) CAR(CDR(CDDR(x)))
 
-/* Integer Object */
-#define INTEGER(x) ((int)(x) >> 3)
-#define MAKE_INTEGER(x) ((LispObject)(((int)(x) << 3) | INTEGER_TAG))
+/* Fixnum */
+#define TO_FIXNUM(x) ((LispObject)(((int)(x) << 3) | FIXNUM_TAG))
+#define theFIXNUM(x) ((int)(x) >> 3)
+#define FIXNUM_P(x) (TAGOF(x) == FIXNUM_TAG)
 
-/* Function Object */
-#define FUNCTION(x) ((x)->u.fun)
-#define LOCAL_ENV(x) (FUNCTION(x)->env)
-#define PRIMITIVE(x) (FUNCTION(x)->prim)
-#define FUNC_DENV(x) (FUNCTION(x)->denv)
-#define FUNC_FLAG(x) (FUNCTION(x)->is_c_func)
-#define BLOCK_ENV(x) (FUNCTION(x)->block_env)
-#define EXPRESSION(x) (FUNCTION(x)->expr)
-#define PARAMETERS(x) (FUNCTION(x)->parms)
-#define FENV(x) (FUNCTION(x)->fenv)
+/* Function */
+#define TO_FUNCTION(x) ((LispObject)((int)(x) | FUNCTION_TAG))
+#define theFUNCTION(x) ((function_t)UNTAG(x))
+#define FUNCTION_P(x) (TAGOF(x) == FUNCTION_TAG)
 
-/* Symbol Object */
-#define MAKE_SYMBOL(x) ((LispObject)((int)(x) | SYMBOL_TAG))
-#define theSYMBOL(x) ((symbol_t)UNTAG(x))
-#define SYMBOL_NAME(x) (theSYMBOL(x)->symbol_name)
+#define ARITY(x) (theFUNCTION(x)->arity)
+#define BLOCK_ENV(x) (theFUNCTION(x)->u.s.block_env)
+#define EXPRESSION(x) (theFUNCTION(x)->u.s.body)
+#define FDEFINITION_ENV(x) (theFUNCTION(x)->u.s.fdefinition_env)
+#define FUNCTION_CFLAG(x) (theFUNCTION(x)->is_C_function)
+#define LEXICAL_ENV(x) (theFUNCTION(x)->u.s.lexical_env)
+#define PARAMETERS(x) (theFUNCTION(x)->u.s.parameters)
+#define PRIMITIVE(x) (theFUNCTION(x)->u.fptr)
 
-/* String Object */
-#define STRING(x) ((x)->u.string)
+/* Hash table */
+#define TO_HASH_TABLE(x) ((HashTable)(x))
+#define theHASH_TABLE(x) ((x)->u.hash_table)
+#define HASH_TABLE_P(x) (POINTER_P(x) && HASH_TABLE == (x)->type)
 
-/* Character Object */
-#define MAKE_CHARACTER(x) ((LispObject)(((int)(x) << 3) | CHARACTER_TAG))
-#define CHARACTER(x) ((int)(x) >> 3)
+#define HASH_CMP(x) (theHASH_TABLE(x)->compare_fn)
+#define TABLE_COUNT(x) (theHASH_TABLE(x)->count)
+#define HASH_FN(x) (theHASH_TABLE(x)->hash_fn)
+#define TABLE_ELEMENTS(x) (theHASH_TABLE(x)->elements)
+#define TABLE_SIZE(x) (theHASH_TABLE(x)->size)
 
-/* Stream Object */
+/* Package */
+#define TO_PACKAGE(x) ((Package)(x))
+#define thePACKAGE(x) ((x)->u.package)
+#define PACKAGE_P(x) (POINTER_P(x) && PACKAGE == (x)->type)
+
+/* Stream */
 #define theSTREAM(x) ((x)->u.stream)
-#define FILE_STREAM(x) ((x)->u.file)
+#define STREAM_P(x) (POINTER_P(x) && STREAM == (x)->type)
 
-/* Vector Object */
+#define STREAM_FILE(x) (theSTREAM(x)->u.file)
+
+/* String */
+#define TO_STRING(x) ((LispObject)((int)(x) | STRING_TAG))
+#define theSTRING(x) ((string_t)UNTAG(x))
+#define STRING_P(x) (TAGOF(x) == STRING_TAG)
+
+#define STRING_CONTENT(x) (theSTRING(x)->content)
+#define STRING_LENGTH(x) (theSTRING(x)->length)
+
+/* Symbol */
+#define TO_SYMBOL(x) ((LispObject)((int)(x) | SYMBOL_TAG))
+#define theSYMBOL(x) ((symbol_t)UNTAG(x))
+#define SYMBOL_P(x) (TAGOF(x) == SYMBOL_TAG)
+
+#define SYMBOL_NAME(x) (theSYMBOL(x)->name)
+#define SYMBOL_PACKAGE(x) (theSYMBOL(x)->package)
+
+/* Vector */
 #define theVector(x) ((x)->u.vector)
-
-#define PHEAD(func_name) LispObject func_name(Cons args)
+#define VECTOR_P(x) (POINTER_P(x) && VECTOR == (x)->type)
 
 #endif
