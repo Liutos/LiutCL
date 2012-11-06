7dd285438db187c3e1e133505ac0bbcfc6a7ef64
LiuTao <mat.liutos@gmail.com>
2012-11-06 23:13:11 +0800

    实现了用于函数调用时进行参数个数检查的函数check_arity_pattern

--- src/eval_sexp.c
+++ src/eval_sexp.c
@@ -14,10 +14,13 @@
 #include "cons.h"
 #include "env_types.h"
 #include "environment.h"
+#include "function.h"
+#include "object.h"
 #include "primitives.h"
 #include "print_sexp.h"
 #include "stream.h"
 #include "types.h"
+#include "vm_stack.h"
 
 #define CALL_EVAL(eval_fn, arg) eval_fn(arg, lenv, denv, benv, fenv)
 #define DEFEVAL(name, hd)                       \
@@ -26,40 +29,14 @@
                     Environment denv,           \
                     BlockEnvironment benv,      \
                     Environment fenv)
+#define DEFSPEC(name, hd) DEFEVAL(name, hd)
 #define eq(x, y) (x == y)
 
-DEFEVAL(eval_progn, _);
 DEFEVAL(eval_cons, _);
+DEFEVAL(eval_progn, _);
 DEFEVAL(eval_sexp, _);
 
 jmp_buf escape;
-
-inline LispObject invoke_C_function(Function C_function, Cons args)
-{
-    return PRIMITIVE(C_function)(args);
-}
-
-LispObject invoke_Lisp_function(Function Lisp_function, Cons args, Environment denv)
-/* Invokes a Lisp function means evaluating the function's body in a extended
-   environment. */
-{
-    BlockEnvironment benv = BLOCK_ENV(Lisp_function);
-    Environment fenv = FDEFINITION_ENV(Lisp_function);
-    Environment lenv =
-        make_new_env(PARAMETERS(Lisp_function),
-                     args,
-                     LEXICAL_ENV(Lisp_function));
-    
-    return CALL_EVAL(eval_progn, EXPRESSION(Lisp_function));
-}
-
-LispObject invoke_function(Function function, Cons args, Environment denv)
-{
-    if (TRUE == FUNCTION_CFLAG(function))
-	return invoke_C_function(function, args);
-    else
-	return invoke_Lisp_function(function, args, denv);
-}
 
 BOOL is_special_operator(LispObject op)
 {
@@ -69,28 +46,32 @@ BOOL is_special_operator(LispObject op)
         lt_defvar,
         lt_function,
         lt_fset,
+        lt_progn,
+#ifndef FS
         lt_if,
         lt_lambda,
-        lt_progn,
         lt_quote,
+#endif
         lt_return_from,
-        lt_set,
+        lt_setq,
         lt_throw,
     };
     int length = sizeof(lt_specs) / sizeof(Symbol);
-    int i;
-    for (i = 0; i < length; ++i)
+
+    for (int i = 0; i < length; ++i)
         if (eq(op, lt_specs[i]))
             return TRUE;
 
     return FALSE;
 }
 
-DEFEVAL(eval_block, argv)
+DEFSPEC(eval_block, argv)
 {
+    Symbol name;
     jmp_buf context;
     int val;
-    Symbol name = FIRST(argv);
+
+    name = FIRST(argv);
     val = setjmp(context);
     if (0 == val)
         return eval_progn(CDR(argv), lenv, denv,
@@ -99,21 +80,27 @@ DEFEVAL(eval_block, argv)
         return (LispObject)val;
 }
 
-DEFEVAL(eval_catch, argv)
+DEFSPEC(eval_catch, argv)
 {
-    jmp_buf context;
+    LispObject tag;
     int val;
-    LispObject tag = CALL_EVAL(eval_sexp, FIRST(argv));
+    jmp_buf context;
+
+    tag = CALL_EVAL(eval_sexp, FIRST(argv));
     /* Simulates the dynamic scope by twice application of memcpy */
     memcpy(context, escape, sizeof(jmp_buf));
     val = setjmp(escape);
     if (0 == val) {
-        LispObject value = CALL_EVAL(eval_progn, CDR(argv));
+        LispObject value;
+
+        value = CALL_EVAL(eval_progn, CDR(argv));
         memcpy(escape, context, sizeof(jmp_buf));
 
         return value;
     } else {                    /* Returns from a invokation of THROW */
-        Cons cons = (Cons)val;
+        Cons cons;
+
+        cons = (Cons)val;
         memcpy(escape, context, sizeof(jmp_buf));
         if (eq(CAR(cons), tag))
             return CDR(cons);
@@ -122,28 +109,32 @@ DEFEVAL(eval_catch, argv)
     }
 }
 
-DEFEVAL(eval_defvar, argv)
+DEFSPEC(eval_defvar, argv)
 {
-    Symbol name = FIRST(argv);
-    if (NULL == get_value(name, global_dynamic_env))
-        global_dynamic_env =
-            extend_env(name,
-                       CALL_EVAL(eval_sexp, SECOND(argv)),
-                       global_dynamic_env);
+    Symbol name;
+
+    name = FIRST(argv);
+    if (NULL == get_value(name, global_dynamic_env)) {
+        LispObject value;
+
+        value = CALL_EVAL(eval_sexp, SECOND(argv));
+        SYMBOL_VALUE(name) = value;
+        global_dynamic_env = extend_env(name, value, global_dynamic_env);
+    }
 
     return name;
 }
 
-DEFEVAL(eval_function, arg)
+DEFSPEC(eval_function, arg)
 {
     return get_value(arg, fenv);
 }
 
-DEFEVAL(eval_progn, exps)
+DEFSPEC(eval_progn, exps)
 {
-    if (is_tail(exps))
+    if (!CONS_P(exps))
 	return lt_nil;
-    if (is_tail(CDR(exps)))
+    if (!CONS_P(CDR(exps)))
 	return CALL_EVAL(eval_sexp, CAR(exps));
     while (!is_tail(exps)) {
 	if (is_tail(CDR(exps)))
@@ -155,10 +146,13 @@ DEFEVAL(eval_progn, exps)
     return CALL_EVAL(eval_sexp, CAR(exps));
 }
 
-DEFEVAL(eval_return_from, argv)
+DEFSPEC(eval_return_from, argv)
 {
-    Symbol name = FIRST(argv);
-    LispObject result = CALL_EVAL(eval_sexp, SECOND(argv));
+    LispObject result;
+    Symbol name;
+
+    result = CALL_EVAL(eval_sexp, SECOND(argv));
+    name = FIRST(argv);
     while (benv != NULL) {
         if (benv->name == name)
             longjmp(benv->context, (int)result);
@@ -168,18 +162,24 @@ DEFEVAL(eval_return_from, argv)
     exit(1);
 }
 
-DEFEVAL(eval_throw, argv)
+DEFSPEC(eval_throw, argv)
 {
-    LispObject tag = CALL_EVAL(eval_sexp, FIRST(argv));
-    LispObject result = CALL_EVAL(eval_sexp, SECOND(argv));
+    LispObject result;
+    LispObject tag;
+
+    result = CALL_EVAL(eval_sexp, SECOND(argv));
+    tag = CALL_EVAL(eval_sexp, FIRST(argv));
     longjmp(escape, (int)make_cons(tag, result));
 }
 
+#ifndef FS
 DEFEVAL(eval_spec, exp)
 {
-    Cons argv = CDR(exp);
-    LispObject op = FIRST(exp);
+    Cons argv;
+    LispObject op;
 
+    argv = CDR(exp);
+    op = FIRST(exp);
     if (eq(op, lt_block))
         return CALL_EVAL(eval_block, argv);
     if (eq(op, lt_catch))
@@ -207,30 +207,83 @@ DEFEVAL(eval_spec, exp)
         return FIRST(argv);
     if (eq(op, lt_return_from))
         return CALL_EVAL(eval_return_from, argv);
-    if (eq(op, lt_set)) {
-        extend_env(FIRST(argv), CALL_EVAL(eval_sexp, SECOND(argv)), lenv);
+    if (eq(op, lt_setq)) {
+        LispObject value;
 
-        return lt_nil;
+        value = CALL_EVAL(eval_sexp, SECOND(argv));
+        extend_env(FIRST(argv), value, lenv);
+
+        return value;
     }
     if (eq(op, lt_throw))
         return CALL_EVAL(eval_throw, argv);
     write_string(standard_error, TO_STRING("WTF!\n"));
     exit(1);
 }
+#endif
 
 /* Argument `parms' must be a proper list. */
-int check_arity(int arity, Cons parms)
+int check_arity(int arity, List parms)
 {
+ LABEL:
     if (0 == arity && eq(lt_nil, parms)) return 0;
     if (0 == arity) return cons_length(parms);
     if (eq(lt_nil, parms)) return arity;
-    return check_arity(arity - 1, CDR(parms));
+    arity--;
+    parms = CDR(parms);
+    goto LABEL;
+    /* return check_arity(arity - 1, CDR(parms)); */
+}
+
+void check_arity_pattern(Arity arity, List args)
+{
+    BOOL key_flag, rest_flag;
+    int key_count, opt_count, req_count;
+
+    key_flag = arity->key_flag;
+    rest_flag = arity->rest_flag;
+    key_count = arity->key_count;
+    req_count = arity->req_count;
+    opt_count = arity->opt_count;
+    for (int i = 0; i < req_count; ++i) {
+        if (!CONS_P(args)) {
+            write_format(standard_error, "Two few arguments\n");
+            exit(1);
+        }
+        args = CDR(args);
+    }
+    if (CONS_P(args) &&
+        0 == opt_count && FALSE == key_flag && FALSE == rest_flag) {
+        write_format(standard_error, "Two many arguments\n");
+        exit(1);
+    }
+    for (int i = 0; i < opt_count; ++i) {
+        if (!CONS_P(args))
+            break;
+        args = CDR(args);
+    }
+    if (CONS_P(args) && FALSE == key_flag && FALSE == rest_flag) {
+        write_format(standard_error, "Two many arguments\n");
+        exit(1);
+    }
+    if (TRUE == rest_flag && FALSE == key_flag)
+        return;
+    for (int i = 0; i < key_count; ++i) {
+        if (CONS_P(args) && !CONS_P(CDR(args))) {
+            write_format(standard_error, "keyword arguments in ($!) should occur pairwise.\n", CAR(args));
+            exit(1);
+        }
+        if (CONS_P(args) && CONS_P(CDR(args)))
+            args = CDDR(args);
+    }
 }
 
 DEFEVAL(eval_operator, op)
 {
     if (SYMBOL_P(op)) {
-	LispObject tmp = get_value(op, fenv);
+	LispObject tmp;
+
+        tmp = get_value(op, fenv);
 	if (tmp != NULL)
 	    return tmp;
 	else {
@@ -243,11 +296,15 @@ DEFEVAL(eval_operator, op)
 
 DEFEVAL(eval_args, args)
 {
-    Cons head, pre, cur;
+    Cons cur, head, pre;
 
     pre = head = make_cons(lt_nil, lt_nil);
     while (!is_tail(args)) {
-        cur = make_cons(CALL_EVAL(eval_sexp, CAR(args)), lt_nil);
+        LispObject val;
+
+        val = CALL_EVAL(eval_sexp, CAR(args));
+        push_object(val);
+        cur = make_cons(val, lt_nil);
         _CDR(pre) = cur;
         pre = cur;
         args = CDR(args);
@@ -258,28 +315,81 @@ DEFEVAL(eval_args, args)
 
 DEFEVAL(eval_cons, exps)
 {
-    Cons args = CDR(exps);
-    LispObject op = FIRST(exps);
+    Cons args;
+    LispObject op;
+
+    args = CDR(exps);
+    op = FIRST(exps);
+#ifdef FS
+    switch (TYPE(op)) {
+    case CONS:
+        op = CALL_EVAL(eval_cons, op);
+        if (!FUNCTION_P(op)) {
+            write_format(standard_error, "%! isn't a functional object.\n", op);
+            exit(1);
+        } else
+            goto LABEL;
+    case SYMBOL:
+        op = CALL_EVAL(eval_operator, op);
+        if (!FUNCTION_P(op)) {
+            write_format(standard_error, "%! isn't a functional object.\n", op);
+            exit(1);
+        }
+    LABEL:
+    case FUNCTION: {
+        /* int result = check_arity(ARITY(op), args); */
+        /* if (result != 0) { */
+        /*     write_format(standard_error, "Wrong number of args in %!\n", op); */
+        /*     exit(1); */
+        /* } else { */
+            if (REGULAR == FTYPE(op))
+                args = CALL_EVAL(eval_args, args);
+            else {
+                Cons tmp;
+
+                tmp = args;
+                while (!is_tail(tmp)) {
+                    push_object(CAR(tmp));
+                    tmp = CDR(tmp);
+                }
+            }
+            check_arity_pattern(ARITY(op), args);
+            /* return invoke_function(op, args, denv); */
+            return invoke_function(op, args, lenv, denv, benv, fenv);
+        /* } */
+    }
+    default :
+        write_format(standard_error, "%! isn't a functional object.\n", op);
+        exit(1);
+    }
+#else
     if (is_special_operator(op))
         return CALL_EVAL(eval_spec, exps);
     else {
-        Function fn = CALL_EVAL(eval_operator, op);
-        int result = check_arity(ARITY(fn), args);
-        if (result != 0) {
-            write_format(standard_error, "Wrong number of args\n");
-            exit(1);
-        } else {
+        /* Regular function application */
+        Function fn;
+
+        fn = CALL_EVAL(eval_operator, op);
+        /* int result = check_arity(ARITY(fn), args); */
+        /* if (result != 0) { */
+        /*     write_format(standard_error, "Wrong number of args in $!\n", fn); */
+        /*     exit(1); */
+        /* } else { */
             args = CALL_EVAL(eval_args, args);
+            check_arity_pattern(ARITY(fn), args);
 
             return invoke_function(fn, args, denv);
-        }
+        /* } */
     }
+#endif
 }
 
 LispObject eval_atom(Atom exp, Environment lenv, Environment denv)
 {
     if (SYMBOL_P(exp)) {
-	LispObject tmp = get_value(exp, lenv);
+	LispObject tmp;
+
+        tmp = get_value(exp, lenv);
 	if (tmp != NULL)
 	    return tmp;
         else if ((tmp = get_value(exp, denv)) != NULL)
