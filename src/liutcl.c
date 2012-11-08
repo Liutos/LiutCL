32c35aa6c89059d2a20509020569442b3e89f14f
LiuTao <mat.liutos@gmail.com>
2012-11-08 23:05:38 +0800

    改为使用init_packages函数对内置的包进行初始化，然后符号表就依附于具体的包了，所以parse_*系列的函数就增加了一个Package类型的参数。

--- src/liutcl.c
+++ src/liutcl.c
@@ -11,34 +11,43 @@
 #include "environment.h"
 #include "eval_sexp.h"
 #include "init.h"
+#include "package.h"
 #include "parse_sexp.h"
 #include "print_sexp.h"
 #include "read_sexp.h"
 #include "stream.h"
+#include "symbol.h"
 #include "types.h"
 
 int main(int argc, char *argv[])
 {
-    Environment lexical_env;
-    Environment fdefinition_env;
-    char *input;
+    BlockEnvironment benv;
+    Environment lenv;
+    Environment fenv;
     LispObject sexp, result;
-    BlockEnvironment block_env;
+    char *input;
 
-    init_special_operators();
+    /* Initialize the global symbol table */
+    init_packages();
+    /* Initialize the global constant environment */
+    global_constant_env = make_empty_env();
+    global_constant_env = init_cvars(global_constant_env);
+    /* Initialize the global dynamic scope environment */
     global_dynamic_env = make_empty_env();
     global_dynamic_env = init_dvars(global_dynamic_env);
-    block_env = NULL;
-    fdefinition_env = make_empty_env();
-    fdefinition_env = init_primitives(fdefinition_env);
-    lexical_env = make_empty_env();
-    lexical_env = init_variables(lexical_env);
+
+    benv = NULL;
+    fenv = make_empty_env();
+    fenv = init_primitives(fenv);
+    lenv = make_empty_env();
+
     do {
-        write_format(standard_output, "LT-USER> ");
+        write_format(standard_output, "CL-USER> ");
         fflush(stdout);
+
         input = read_sexp(stdin);
-        sexp = parse_sexp(input);
-        result = eval_sexp(sexp, lexical_env, global_dynamic_env, block_env, fdefinition_env);
+        sexp = parse_sexp(input, pkg_cl);
+        result = eval_sexp(sexp, lenv, global_dynamic_env, benv, fenv);
         print_object(result, standard_output);
     } while (1);
 
