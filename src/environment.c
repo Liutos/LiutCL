32c384d4a072dffd3113f7cb08267e8d1d43789a
LiuTao <mat.liutos@gmail.com>
2012-11-08 23:13:03 +0800

    一、extend_env_cons改名为extend_env_by_cons 二、将对ensure_symbol_exists函数改为gen_pkg_sym的调用

--- src/environment.c
+++ src/environment.c
@@ -8,19 +8,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "atom_proc.h"
+#include "atom.h"
 #include "cons.h"
 #include "env_types.h"
+#include "package.h"
 #include "print_sexp.h"
 #include "stream.h"
-#include "symbol_table.h"
+#include "symbol.h"
 #include "types.h"
 
+Environment global_constant_env;
 Environment global_dynamic_env;
 
 LispObject get_value_aux(Symbol symbol, Environment env)
 {
-    env_entry_t map = env->map;
+    env_entry_t map;
+
+    map = env->map;
     while (map != NULL) {
 	if (map->symbol == symbol)
 	    return map->value;
@@ -33,7 +37,9 @@ LispObject get_value_aux(Symbol symbol, Environment env)
 LispObject get_value(Symbol symbol, Environment env)
 {
     while (env != NULL) {
-	LispObject value = get_value_aux(symbol, env);
+	LispObject value;
+
+        value = get_value_aux(symbol, env);
 	if (value != NULL)
 	    return value;
 	env = env->next;
@@ -44,7 +50,9 @@ LispObject get_value(Symbol symbol, Environment env)
 
 Environment extend_env(Symbol symbol, LispObject value, Environment env)
 {
-    env_entry_t node = malloc(sizeof(struct env_entry_t));
+    env_entry_t node;
+
+    node = malloc(sizeof(struct env_entry_t));
     node->symbol = symbol;
     node->value = value;
     node->next = env->map->next;
@@ -53,10 +61,10 @@ Environment extend_env(Symbol symbol, LispObject value, Environment env)
     return env;
 }
 
-Environment extend_env_cons(Cons symbols, Cons values, Environment env)
+Environment extend_env_by_cons(Cons symbols, Cons values, Environment env)
 {
-    while (!is_tail(symbols)) {
-	if (is_tail(values)) {
+    while (!TAIL_P(symbols)) {
+	if (TAIL_P(values)) {
 	    fprintf(stderr, "Too less values.\n");
 	    exit(1);
 	}
@@ -64,7 +72,7 @@ Environment extend_env_cons(Cons symbols, Cons values, Environment env)
 	symbols = CDR(symbols);
 	values = CDR(values);
     }
-    if (is_tail(values))
+    if (TAIL_P(values))
 	return env;
     else {
 	fprintf(stderr, "Too much symbols.\n");
@@ -72,9 +80,9 @@ Environment extend_env_cons(Cons symbols, Cons values, Environment env)
     }
 }
 
-Environment extend_env_by_name(char *name, LispObject value, Environment env)
+Environment extend_env_by_name(char *name, Package pkg, LispObject value, Environment env)
 {
-    return extend_env(ensure_symbol_exists(name), value, env);
+    return extend_env(gen_pkg_sym(name, pkg), value, env);
 }
 
 Environment make_empty_env(void)
@@ -91,15 +99,15 @@ Environment make_empty_env(void)
 
 void describe_env_aux(Environment env, Stream stream)
 {
-    env_entry_t map = env->map->next;
+    env_entry_t map;
+
+    map = env->map->next;
     while (map != NULL) {
-	print_atom(map->symbol, stream);
-	printf("\t->\t");
-	if (is_atom_object(map->value))
+        write_format(stream, "$!\t->\t", map->symbol);
+	if (!CONS_P(map->value))
 	    print_atom(map->value, stream);
 	else
 	    printf("%p", map->value);
-	/* putchar('\n'); */
         write_char(standard_output, TO_CHAR('\n'));
 	map = map->next;
     }
@@ -115,17 +123,19 @@ void describe_env(Environment env, Stream stream)
 
 Environment make_new_env(Cons parms, Cons values, Environment prev_env)
 {
-    Environment env = make_empty_env();
-    env->next = prev_env;
-    env = extend_env_cons(parms, values, env);
+    Environment env;
 
-    return env;
+    env = make_empty_env();
+    env->next = prev_env;
+
+    return extend_env_by_cons(parms, values, env);
 }
 
 BlockEnvironment make_block_env(Symbol name, jmp_buf context, BlockEnvironment prev_block_env)
 {
-    BlockEnvironment block_env = malloc(sizeof(struct block_environment_t));
+    BlockEnvironment block_env;
 
+    block_env = malloc(sizeof(struct block_environment_t));
     block_env->name = name;
     *block_env->context = *context;
     block_env->prev = prev_block_env;
