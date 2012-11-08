57ff3dabd6d46166a0a21c7f25d5d69f82b1f68a
LiuTao <mat.liutos@gmail.com>
2012-11-08 23:11:52 +0800

    增加init_packages函数声明

--- src/init.h
+++ src/init.h
@@ -4,8 +4,8 @@
 #include "env_types.h"
 
 extern Environment init_dvars(Environment);
+extern Environment init_cvars(Environment);
 extern Environment init_primitives(Environment);
-extern Environment init_variables(Environment);
-extern void init_special_operators(void);
+extern void init_packages(void);
 
 #endif
