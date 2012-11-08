7568ef232c1fe8de849f60bc6de22675e26d0eb5
LiuTao <mat.liutos@gmail.com>
2012-11-08 23:10:28 +0800

    即原来的atom_proc.h文件

---
+++ src/atom.h
@@ -0,0 +1,15 @@
+#ifndef ATOM_H
+#define ATOM_H
+
+#include "env_types.h"
+#include "types.h"
+
+extern BOOL is_atom_object(LispObject);
+extern BOOL is_symbol(LispObject);
+extern BOOL is_tail(LispObject);
+extern BOOL is_true_obj(LispObject);
+extern Character make_char(char);
+extern Fixnum make_fixnum(int);
+extern String make_string(char *);
+
+#endif
