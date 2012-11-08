1e2d4de077059ae49cc449427dc3cf60cd01ec6d
LiuTao <mat.liutos@gmail.com>
2012-11-08 23:10:42 +0800

    即原来的atom_proc.c文件

---
+++ src/atom.c
@@ -0,0 +1,56 @@
+/*
+ * atom_proc.c
+ *
+ * 
+ *
+ * Copyright (C) 2012-10-05 liutos
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "cons.h"
+#include "env_types.h"
+#include "object.h"
+#include "symbol_table.h"
+#include "types.h"
+
+Character make_char(char C_character)
+{
+    return TO_CHAR(C_character);
+}
+
+Fixnum make_fixnum(int C_integer)
+{
+    return TO_FIXNUM(C_integer);
+}
+
+String make_string(char *C_string)
+{
+    string_t object = malloc(sizeof(struct string_t));
+
+    object->content = strdup(C_string);
+    object->length = strlen(C_string);
+
+    return TO_STRING(object);
+}
+
+BOOL is_true_obj(LispObject obj)
+{
+    return lt_nil != obj;
+}
+
+BOOL is_atom_object(LispObject object)
+{
+    return !CONS_P(object);
+}
+
+BOOL is_tail(LispObject object)
+{
+    return is_atom_object(object);
+}
+
+BOOL is_symbol(LispObject object)
+{
+    return SYMBOL_P(object);
+}
