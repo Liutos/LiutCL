d6c8e779a2c424377c94c4ef96a1071854ef34c8
LiuTao <mat.liutos@gmail.com>
2012-11-08 23:13:08 +0800

    *** empty log message ***

--- src/environment.h
+++ src/environment.h
@@ -9,12 +9,13 @@
 extern BlockEnvironment make_block_env(Symbol, jmp_buf, BlockEnvironment);
 extern Environment extend_cons_binding(Cons, Cons, Environment);
 extern Environment extend_env(Symbol, LispObject, Environment);
-extern Environment extend_env_by_name(char *, LispObject, Environment);
+extern Environment extend_env_by_name(char *, Package, LispObject, Environment);
 extern Environment make_empty_env(void);
 extern Environment make_new_env(Cons, Cons, Environment);
 extern LispObject get_value(Symbol, Environment);
 extern void describe_env(Environment, Stream);
 
+extern Environment global_constant_env;
 extern Environment global_dynamic_env;
 
 #endif
