875d603457131ea993f0e8f0f44d11defa1cf5b8
LiuTao <mat.liutos@gmail.com>
2012-11-09 16:15:21 +0800

    增加声明

--- src/data.h
+++ src/data.h
@@ -14,10 +14,14 @@
 extern PHEAD(lt_car);
 extern PHEAD(lt_cdr);
 extern PHEAD(lt_cons);
+extern PHEAD(lt_list);
 extern PHEAD(lt_rplaca);
 extern PHEAD(lt_rplacd);
 /* Package */
 extern PHEAD(lt_find_package);
+extern PHEAD(lt_package_name);
+/* String */
+extern PHEAD(lt_char);
 /* Others */
 extern PHEAD(lt_eq);
 extern PHEAD(lt_special_operator_p);
