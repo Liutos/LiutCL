a681de3d4c8e0dd88e85a9522ecf1b1c1ea4e0c4
LiuTao <mat.liutos@gmail.com>
2012-11-09 16:18:55 +0800

    *** empty log message ***

--- src/function.h
+++ src/function.h
@@ -1,11 +1,13 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
 
+#include "macro_def.h"
 #include "types.h"
 
 extern Arity make_arity_t(int, int, BOOL, BOOL, int, List);
 extern Function make_C_function(primitive_t, Arity, FunctionType);
-extern Function make_Lisp_function(List parms, LispObject expr, Environment lenv, Environment denv, BlockEnvironment benv, Environment fenv);
-extern LispObject invoke_function(Function function, Cons args, Environment lenv, Environment denv, BlockEnvironment benv, Environment fenv);
+extern O2E4(make_Lisp_function);
+extern O2E4(make_Lisp_macro);
+extern O2E4(invoke_function);
 
 #endif
