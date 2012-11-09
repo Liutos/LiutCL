0559ba7f551617a4b50b71e6986c29f3e94e236d
LiuTao <mat.liutos@gmail.com>
2012-11-09 16:14:21 +0800

    裁剪掉不用的宏

--- src/pdecls.h
+++ src/pdecls.h
@@ -1,29 +1,20 @@
 #ifndef PDECLS_H
 #define PDECLS_H
 
-#include "edecls.h"
+#include "macro_def.h"
 
 #define ARGS args
 #define ARG1 FIRST(args)
 #define ARG2 SECOND(args)
 #define ARG3 THIRD(args)
 
-#define ACCESS_PARM2(o1, ac1, o2, ac2)          \
-    do {                                        \
-        o1 = ac1(ARG1);                         \
-        o2 = ac2(ARG2);                         \
-    } while (0)
-#define PARM2(o1, o2)                           \
-    do {                                        \
-        o1 = ARG1;                              \
-        o2 = ARG2;                              \
-    } while(0)
 #define PHEAD(fn_name)                          \
     LispObject fn_name(Cons args,               \
                        Environment lenv,        \
                        Environment denv,        \
+                       Environment fenv,        \
                        BlockEnvironment benv,   \
-                       Environment fenv)
+                       GoEnvironment genv)
 #define RETURN(x) return (x)
 
 #endif
