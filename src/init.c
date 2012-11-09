53d38bb5c3a3c18aafa810005e9d1a228db1a3e4
LiuTao <mat.liutos@gmail.com>
2012-11-09 16:13:28 +0800

    增即了一些原语

--- src/init.c
+++ src/init.c
@@ -79,6 +79,7 @@ Environment init_primitives(Environment env)
 {
     Arity req1 = make_arity_t(1, 0, FALSE, FALSE, 0, lt_nil);
     Arity req1opt2 = make_arity_t(1, 2, FALSE, FALSE, 0, lt_nil);
+    Arity req1opt4 = make_arity_t(1, 4, FALSE, FALSE, 0, lt_nil);
     Arity req1rest = make_arity_t(1, 0, TRUE, FALSE, 0, lt_nil);
     Arity req2 = make_arity_t(2, 0, FALSE, FALSE, 0, lt_nil);
     Arity req2opt1 = make_arity_t(2, 1, FALSE, FALSE, 0, lt_nil);
@@ -95,28 +96,36 @@ Environment init_primitives(Environment env)
     freg("EQ", pkg_cl, lt_eq, req2);
     freg("SPECIAL-OPERATOR-P", pkg_cl, lt_special_operator_p, req1);
     freg("TYPE-OF", pkg_cl, lt_type_of, req1);
-    freg("READ-A-CHAR", pkg_cl, lt_read_a_char, req1);
     freg("WRITE-A-CHAR", pkg_cl, lt_write_a_char, req1);
     /* Cons operations */
     freg("CAR", pkg_cl, lt_car, req1);
     freg("CDR", pkg_cl, lt_cdr, req1);
     freg("CONS", pkg_cl, lt_cons, req2);
+    freg("LIST", pkg_cl, lt_list, rest);
     freg("RPLACA", pkg_cl, lt_rplaca, req2);
     freg("REPACD", pkg_cl, lt_rplacd, req2);
+    /* IO operations */
+    freg("READ-CHAR", pkg_cl, lt_read_char, req1opt4);
     /* Package operations */
     freg("FIND-PACKAGE", pkg_cl, lt_find_package, req1);
+    freg("PACKAGE-NAME", pkg_cl, lt_package_name, req1);
+    /* String operations */
+    freg("CHAR", pkg_cl, lt_char, req2);
     /* Register special operators */
     sreg("BLOCK", pkg_cl, lt_block, req1rest);
     sreg("CATCH", pkg_cl, lt_catch, req1rest);
     sreg("DEFVAR", pkg_cl, lt_defvar, req1opt2);
     sreg("FSET", pkg_cl, lt_fset, req2);
     sreg("FUNCTION", pkg_cl, lt_function, req1);
+    sreg("GO", pkg_cl, lt_go, req1);
     sreg("IF", pkg_cl, lt_if, req2opt1);
     sreg("LAMBDA", pkg_cl, lt_lambda, req1rest);
+    sreg("MK-MACRO", pkg_cl, lt_mk_macro, req1rest);
     sreg("PROGN", pkg_cl, lt_progn, rest);
     sreg("QUOTE", pkg_cl, lt_quote, req1);
-    sreg("SETQ", pkg_cl, lt_setq, rest);
     sreg("RETURN-FROM", pkg_cl, lt_return_from, req2);
+    sreg("SETQ", pkg_cl, lt_setq, rest);
+    sreg("TAGBODY", pkg_cl, lt_tagbody, rest);
     sreg("THROW", pkg_cl, lt_throw, req2);
 
     return env;
