1de1f3f686d0da102279f8c320ded9c817c6bb10
LiuTao <mat.liutos@gmail.com>
2012-11-09 16:16:03 +0800

    修改了primitive_t的定义

--- src/decls.h
+++ src/decls.h
@@ -4,6 +4,7 @@
 typedef enum bool_t BOOL;
 typedef struct block_environment_t *BlockEnvironment;
 typedef struct environment_t *Environment;
+typedef struct go_environment_t *GoEnvironment;
 typedef struct lisp_object_t *LispObject;
 typedef LispObject Atom;
 typedef LispObject Boolean;
@@ -16,11 +17,12 @@ typedef LispObject HashTable;
 /* Type `List' means proper list */
 typedef LispObject List;
 typedef LispObject Package;
+typedef LispObject Ratio;
 typedef LispObject Stream;
 typedef LispObject String;
 typedef LispObject Symbol;
 typedef LispObject Vector;
-typedef LispObject (*primitive_t)(LispObject, Environment, Environment, BlockEnvironment, Environment);
+typedef LispObject (*primitive_t)(LispObject, Environment, Environment, Environment, BlockEnvironment, GoEnvironment);
 typedef struct env_entry_t *env_entry_t;
 
 extern Symbol lt_nil;
