5e937e89c8f98220e5db0acc08ad81e02190d0b8
LiuTao <mat.liutos@gmail.com>
2012-11-09 16:19:08 +0800

    没有用了

--- src/edecls.h
+++ src/edecls.h
@@ -1,12 +1,4 @@
 #ifndef EDECLS_H
 #define EDECLS_H
 
-#define CALL_EVAL(eval_fn, arg) eval_fn(arg, lenv, denv, benv, fenv)
-#define DEFEVAL(name, hd)                       \
-    LispObject name(LispObject hd,              \
-                    Environment lenv,           \
-                    Environment denv,           \
-                    BlockEnvironment benv,      \
-                    Environment fenv)
-
 #endif
