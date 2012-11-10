02cb86b763b2431366cdeac9f99bd6e5e72535a6
LiuTao <mat.liutos@gmail.com>
2012-11-11 00:38:03 +0800

    将环境类型Environment中用于建立映射的类型修改为哈希表，并修改相应的必要函数的定义，如make_empty_new和get_value等等，建立在hash_table.c文件提供的操作上。

--- src/environment.c
+++ src/environment.c
@@ -1,16 +1,18 @@
 /*
  * environment.c
  *
- * 
+ * Creation, extension and search operations on environment.
  *
  * Copyright (C) 2012-10-05 liutos
  */
+#include <setjmp.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "atom.h"
 #include "cons.h"
-#include "env_types.h"
+#include "eval_sexp.h"
+#include "hash_table.h"
 #include "package.h"
 #include "print_sexp.h"
 #include "stream.h"
@@ -20,15 +22,30 @@
 Environment global_constant_env;
 Environment global_dynamic_env;
 
-LispObject get_value_aux(Symbol symbol, Environment env)
+table_entry_t get_value_aux(Symbol symbol, Environment env)
 {
-    env_entry_t map;
+    /* table_entry_t ent; */
 
-    map = env->map;
-    while (map != NULL) {
-	if (map->symbol == symbol)
-	    return map->value;
-	map = map->next;
+    /* ent = env->map; */
+    /* while (ent != NULL) { */
+    /*     if (ent->symbol == symbol) */
+    /*         return ent; */
+    /*     ent = ent->next; */
+    /* } */
+
+    /* return NULL; */
+    return find_table_entry(symbol, env->map);
+}
+
+table_entry_t get_entry(Symbol var, Environment env)
+{
+    while (env != NULL) {
+	table_entry_t ent;
+
+        ent = get_value_aux(var, env);
+	if (ent != NULL)
+	    return ent;
+	env = env->next;
     }
 
     return NULL;
@@ -36,36 +53,36 @@ LispObject get_value_aux(Symbol symbol, Environment env)
 
 LispObject get_value(Symbol symbol, Environment env)
 {
-    while (env != NULL) {
-	LispObject value;
+    table_entry_t ent;
 
-        value = get_value_aux(symbol, env);
-	if (value != NULL)
-	    return value;
-	env = env->next;
-    }
-
-    return NULL;
+    ent = get_entry(symbol, env);
+    if (ent != NULL)
+        return (LispObject)(ent->value);
+    else
+        return NULL;
 }
 
 Environment extend_env(Symbol symbol, LispObject value, Environment env)
 {
-    env_entry_t node;
+    /* env_entry_t node; */
 
-    node = malloc(sizeof(struct env_entry_t));
-    node->symbol = symbol;
-    node->value = value;
-    node->next = env->map->next;
-    env->map->next = node;
+    /* node = malloc(sizeof(struct env_entry_t)); */
+    /* node->symbol = symbol; */
+    /* node->value = value; */
+    /* node->next = env->map->next; */
+    /* env->map->next = node; */
+
+    /* return env; */
+    add_key_value(symbol, value, env->map);
 
     return env;
 }
 
-Environment extend_env_by_cons(Cons symbols, Cons values, Environment env)
+Environment extend_env_by_kvs(Cons symbols, Cons values, Environment env)
 {
     while (!TAIL_P(symbols)) {
 	if (TAIL_P(values)) {
-	    fprintf(stderr, "Too less values.\n");
+	    error_format("Too less values.\n");
 	    exit(1);
 	}
 	env = extend_env(CAR(symbols), CAR(values), env);
@@ -75,8 +92,9 @@ Environment extend_env_by_cons(Cons symbols, Cons values, Environment env)
     if (TAIL_P(values))
 	return env;
     else {
-	fprintf(stderr, "Too much symbols.\n");
-	exit(1);
+	error_format("Too much symbols.\n");
+	/* exit(1); */
+        longjmp(toplevel, 1);
     }
 }
 
@@ -90,34 +108,54 @@ Environment make_empty_env(void)
     Environment env;
 
     env = malloc(sizeof(struct environment_t));
-    env->map = malloc(sizeof(struct env_entry_t));
-    env->map->next = NULL;
+    env->map = make_hash_table_aux(47, hash_ptr, ptr_cmp);
+    /* env->map = malloc(sizeof(struct env_entry_t)); */
+    /* env->map->next = NULL; */
     env->next = NULL;
 
     return env;
 }
 
+void print_kv(void *key, void *value)
+{
+    Symbol sym;
+    LispObject val;
+
+    sym = (Symbol)key;
+    val = (LispObject)value;
+    write_format(standard_output, "$!\t->\t", sym);
+    if (!CONS_P(val))
+        print_atom(val, standard_output);
+    else
+        write_address(standard_output, val);
+    write_char(standard_output, TO_CHAR('\n'));
+}
+
 void describe_env_aux(Environment env, Stream stream)
 {
-    env_entry_t map;
+    hash_table_t map;
 
-    map = env->map->next;
-    while (map != NULL) {
-        write_format(stream, "$!\t->\t", map->symbol);
-	if (!CONS_P(map->value))
-	    print_atom(map->value, stream);
-	else
-	    printf("%p", map->value);
-        write_char(standard_output, TO_CHAR('\n'));
-	map = map->next;
-    }
+    map = env->map;
+    hash_table_iterate(map, print_kv);
+    /* env_entry_t map; */
+
+    /* map = env->map->next; */
+    /* while (map != NULL) { */
+    /*     write_format(stream, "$!\t->\t", map->symbol); */
+    /*     if (!CONS_P(map->value)) */
+    /*         print_atom(map->value, stream); */
+    /*     else */
+    /*         printf("%p", map->value); */
+    /*     write_char(standard_output, make_char('\n')); */
+    /*     map = map->next; */
+    /* } */
 }
 
 void describe_env(Environment env, Stream stream)
 {
     while (env != NULL) {
-	describe_env_aux(env, stream);
-	env = env->next;
+        describe_env_aux(env, stream);
+        env = env->next;
     }
 }
 
@@ -128,7 +166,7 @@ Environment make_new_env(Cons parms, Cons values, Environment prev_env)
     env = make_empty_env();
     env->next = prev_env;
 
-    return extend_env_by_cons(parms, values, env);
+    return extend_env_by_kvs(parms, values, env);
 }
 
 BlockEnvironment make_block_env(Symbol name, jmp_buf context, BlockEnvironment prev_block_env)
@@ -167,4 +205,15 @@ BOOL is_go_able(LispObject tag, GoEnvironment genv)
     }
 
     return FALSE;
+}
+
+void update_env(Symbol var, LispObject val, Environment env)
+{
+    table_entry_t ent;
+
+    ent = get_entry(var, env);
+    if (ent != NULL)
+        ent->value = val;
+    else
+        extend_env(var, val, env);
 }
