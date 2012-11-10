c8cec8dbd00c202552c813a023ba22bbae502b9e
LiuTao <mat.liutos@gmail.com>
2012-11-11 00:50:54 +0800

    nth_cdr和nth_car等一些支持其它地方代码的函数

--- src/cons.c
+++ src/cons.c
@@ -1,7 +1,7 @@
 /*
  * cons.c
  *
- * 
+ * Construction and other operations on cons objects.
  *
  * Copyright (C) 2012-10-04 liutos
  */
@@ -10,7 +10,36 @@
 
 #include "atom.h"
 #include "object.h"
+#include "stream.h"
 #include "types.h"
+
+unsigned int cons_length(Cons cons)
+{
+    unsigned int length;
+
+    for (length = 0; !eq(lt_nil, cons); cons = CDR(cons))
+        length++;
+
+    return length;
+}
+
+void free_cons(Cons cons)
+{
+    assert(CONS_P(cons));
+
+    free(theCONS(cons));
+}
+
+LispObject get_by_key(LispObject key, List list)
+{
+    while (CONS_P(list)) {
+        if (eq(key, CAR(list)))
+            return CAR(CDR(list));
+        list = CDR(CDR(list));
+    }
+
+    return lt_nil;
+}
 
 Cons make_cons(LispObject car, LispObject cdr)
 {
@@ -21,15 +50,44 @@ Cons make_cons(LispObject car, LispObject cdr)
     return TO_CONS(cons);
 }
 
-void free_cons(Cons cons)
+List make_list(int size, LispObject init_value)
 {
-    assert(CONS_P(cons));
+    Cons cur, pre, head;
 
-    free(theCONS(cons));
+    pre = head = make_cons(lt_nil, lt_nil);
+    while (size-- != 0) {
+        cur = make_cons(init_value, lt_nil);
+        _CDR(pre) = cur;
+        pre = cur;
+    }
+
+    return CDR(head);
 }
 
-unsigned int cons_length(Cons cons)
+LispObject nth_car(unsigned int index, List list)
 {
-    if (lt_nil == cons) return 0;
-    else return 1 + cons_length(CDR(cons));
+    while (index-- != 0)
+        list = CDR(list);
+
+    return CAR(list);
+}
+
+Cons nth_cdr(unsigned int n, List list)
+{
+    while (n-- != 0)
+        list = CDR(list);
+
+    return list;
+}
+
+void set_nth(unsigned int index, List list, LispObject value)
+{
+    while (index-- != 0) {
+        if (!CONS_P(list)) {
+            error_format("Index too large.\n");
+            longjmp(toplevel, 1);
+        }
+        list = CDR(list);
+    }
+    _CAR(list) = value;
 }
