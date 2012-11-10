6b2c17f773c8d9024e95ea4494ca49146d7ed8c7
LiuTao <mat.liutos@gmail.com>
2012-11-11 00:47:15 +0800

    把恶心的O2E4改为DEFMK，代表define maker，同时修改必要的类型信息，例如DEFINVOKE中的第二个参数为Frame类型。

--- src/macro_def.h
+++ src/macro_def.h
@@ -1,18 +1,18 @@
 /*
  * macro_def.h
  *
- *
+ * Definition of macros about define and call.
  *
  * Copyright (C) 2012-11-09 liutos <mat.liutos@gmail.com>
  */
 #ifndef MACRO_DEF_H
 #define MACRO_DEF_H
 
-#define CALL_EVAL(eval_fn, arg) \
+#define CALL_EVAL(eval_fn, arg)                 \
     eval_fn(arg, lenv, denv, fenv, benv, genv)
-#define CALL_INVOKE(invoker, fn, args)          \
+#define CALL_INVOKE(invoker, fn, args)                  \
     invoker(fn, args, lenv, denv, fenv, benv, genv)
-#define CALL_MK(maker, arg1, arg2) \
+#define CALL_MK(maker, arg1, arg2)                      \
     maker(arg1, arg2, lenv, denv, fenv, benv, genv)
 #define DEFEVAL(name, hd)                       \
     LispObject name(LispObject hd,              \
@@ -23,12 +23,19 @@
                     GoEnvironment genv)
 #define DEFINVOKE(fn_name, fn)                  \
     LispObject fn_name(Function fn,             \
-                       List args,               \
+                       Frame args,              \
                        Environment lenv,        \
                        Environment denv,        \
                        Environment fenv,        \
                        BlockEnvironment benv,   \
                        GoEnvironment genv)
-#define O2E4(fn_name) DEFINVOKE(fn_name, _)
+#define DEFMK(mk_name)                          \
+    LispObject mk_name(List parms,              \
+                       List expr,               \
+                       Environment lenv,        \
+                       Environment denv,        \
+                       Environment fenv,        \
+                       BlockEnvironment benv,   \
+                       GoEnvironment genv)
 
 #endif
