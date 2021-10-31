d83c618d6a695844f113a9dfc0382c5196ca262c
Liutos <mat.liutos@gmail.com>
2021-10-31 21:48:40 +0800

    feat: 实现对变量类型的计算。

--- src/typing.lisp
+++ src/typing.lisp
@@ -4,23 +4,36 @@
   'integer
   "233-lisp中的整数类型。")
 
-(defun check-233-type (expr)
+(defparameter *testing-tenv*
+  (let ((tenv '()))
+    (setf tenv (extend tenv 'foo *233-type-integer*))
+    tenv)
+  "单元测试专用的类型环境。")
+
+(defun check-233-type (expr tenv)
   (when (integerp expr)
     (return-from check-233-type *233-type-integer*))
+
+  (when (symbolp expr)
+    (let ((type (lookup tenv expr)))
+      (unless type
+        (error "未定义的变量~A" expr))
+
+      (return-from check-233-type type)))
 
   (assert (consp expr))
   (let ((func (first expr)))
     (when (eq func 'progn)
       (let (last)
         (dolist (e (rest expr))
-          (setf last (check-233-type e)))
+          (setf last (check-233-type e tenv)))
         (return-from check-233-type last)))
 
     (when (member func '(+ - * /))
       (destructuring-bind (lhs rhs)
           (rest expr)
-        (let ((lhs-type (check-233-type lhs))
-              (rhs-type (check-233-type rhs)))
+        (let ((lhs-type (check-233-type lhs tenv))
+              (rhs-type (check-233-type rhs tenv)))
           (unless (equal lhs-type *233-type-integer*)
             (error "要求类型~A，但表达式~A的类型为~A" *233-type-integer* lhs lhs-type))
 
--- t/typing.lisp
+++ t/typing.lisp
@@ -3,7 +3,8 @@
 (defun read-and-check-type (source-code)
   (check-type source-code string)
   (com.liutos.liutcl.interpreter::check-233-type
-   (com.liutos.liutcl.interpreter::read-source-code-from-string source-code)))
+   (com.liutos.liutcl.interpreter::read-source-code-from-string source-code)
+   com.liutos.liutcl.interpreter::*testing-tenv*))
 
 (test check-233-type
   "测试233-lisp的静态类型检查机制。"
@@ -11,4 +12,5 @@
   (is (equal (read-and-check-type "(+ 1 (+ 2 3))") com.liutos.liutcl.interpreter::*233-type-integer*))
   (is (equal (read-and-check-type "(- 1 (- 2 3))") com.liutos.liutcl.interpreter::*233-type-integer*))
   (is (equal (read-and-check-type "(* 1 (* 2 3))") com.liutos.liutcl.interpreter::*233-type-integer*))
-  (is (equal (read-and-check-type "(/ 1 (/ 2 3))") com.liutos.liutcl.interpreter::*233-type-integer*)))
+  (is (equal (read-and-check-type "(/ 1 (/ 2 3))") com.liutos.liutcl.interpreter::*233-type-integer*))
+  (is (equal (read-and-check-type "foo") com.liutos.liutcl.interpreter::*233-type-integer*)))
